c0237f2d423a2f504e2ad3b7e6e4fd8fee254985
Tom Harrison <tharrison@hugeinc.com>
2016-11-30 17:54:03 -0500

    [DSR-143] Updating build pipeline
    * Updated to to standard Java indentation
    * Added TODO notes around Slack & SSH credentials
    * Add log rotation
    * Documented the expected Jenkins plugins

--- ci.groovy
+++ ci.groovy
@@ -1,6 +1,15 @@
 /*
  *
- * Jobs to configure, lint, test, and build artifacts.
+ * Jobs to configure, lint, test, build, and deploy Thorium.
+ *
+ * Plugins required:
+ *   Copy Artifacts
+ *   Credentials
+ *   Git
+ *   Slack
+ *
+ * Plugins suggested:
+ *   ansicolor
  *
  */
 
@@ -10,24 +19,24 @@
  * all dependencies.
  */
 createJenkinsJob('telus-thorium--configure') {
-    triggers {
-	githubPush()
-    }
+  triggers {
+    githubPush()
+  }
 
-    scm {
-      git {
-	remote {
-	  github('telusdigital/telus-thorium-core', 'ssh')
-	  credentials('jenkins')
-	  branch 'master'
-	}
-	extensions {
-	    cleanBeforeCheckout()
-	}
+  scm {
+    git {
+      remote {
+        github('telusdigital/telus-thorium-core', 'ssh')
+        credentials('jenkins')
+        branch 'master'
+      }
+      extensions {
+        cleanBeforeCheckout()
       }
     }
+  }
 
-    steps {
+  steps {
 	shell '''
 	  cd \${WORKSPACE}/core
 	  npm install
@@ -46,16 +55,16 @@ createJenkinsJob('telus-thorium--configure') {
 	  npm install
 	  npm rebuild node-sass
 	'''.stripIndent().trim()
+  }
+
+  publishers {
+    archiveArtifacts {
+      pattern('**/*')
+      onlyIfSuccessful()
     }
 
-    publishers {
-	archiveArtifacts {
-	    pattern('**/*')
-	    onlyIfSuccessful()
-	}
-
-	downstream 'telus-thorium--test'
-    }
+    downstream 'telus-thorium--test'
+  }
 }
 
 /**
@@ -63,20 +72,19 @@ createJenkinsJob('telus-thorium--configure') {
  * linters and tests.
  */
 createJenkinsJob('telus-thorium--test') {
-    steps {
-	copyArtifacts('telus-thorium--configure') {
-	    buildSelector {
-		latestSuccessful(true)
-	    }
-	}
-	shell("cd ${WORKSPACE}/core && npm lint")
-	shell("cd ${WORKSPACE}/enriched && npm lint && npm test")
-	shell("cd ${WORKSPACE}/docs && npm lint")
+  steps {
+    copyArtifacts('telus-thorium--configure') {
+      buildSelector {
+        latestSuccessful(true)
+      }
     }
+    shell("npm run lint")
+    shell("npm test")
+  }
 
-    publishers {
-	downstream 'telus-thorium--build'
-    }
+  publishers {
+    downstream 'telus-thorium--build'
+  }
 }
 
 /**
@@ -84,21 +92,21 @@ createJenkinsJob('telus-thorium--test') {
  * the documentation site's static HTML, CSS, JS, and assets.
  */
 createJenkinsJob('telus-thorium--build') {
-    steps {
-	copyArtifacts('telus-thorium--configure') {
-	    buildSelector {
-		latestSuccessful(true)
-	    }
-	}
-	shell("npm run build")
+  steps {
+    copyArtifacts('telus-thorium--configure') {
+      buildSelector {
+        latestSuccessful(true)
+      }
     }
+    shell("npm run build")
+  }
 
-    publishers {
-	archiveArtifacts {
-	    pattern('docs/dist/docs/**/*')
-	    onlyIfSuccessful()
-	}
+  publishers {
+    archiveArtifacts {
+      pattern('docs/dist/docs/**/*')
+      onlyIfSuccessful()
     }
+  }
 }
 
 /*
@@ -112,10 +120,10 @@ createJenkinsJob('telus-thorium--build') {
  * development environment web server. It takes those files from its
  * upstream job, which is telus-thorium--build.
  */
-createJenkinsDeployJob('telus-thorium--deploy-dev', 'jenkins@example.com:/var/www/versions/dev/', 'upstream') {
-    triggers {
-	upstream('telus-thorium--build')
-    }
+createJenkinsDeployJob('telus-thorium--deploy-dev', 'jenkins@example.com:/var/www/versions/dev/', 'telus-thorium--build') {
+  triggers {
+    upstream('telus-thorium--build')
+  }
 }
 
 /**
@@ -123,21 +131,17 @@ createJenkinsDeployJob('telus-thorium--deploy-dev', 'jenkins@example.com:/var/ww
  * web server. It takes those artifacts from its upstream job, which
  * is dev.
  */
-createJenkinsDeployJob('telus-thorium--deploy-qa', 'jenkins@example.com:/var/www/versions/qa/', 'upstream') {
-    triggers {
-	upstream('telus-thorium--deploy-dev')
-    }
+createJenkinsDeployJob('telus-thorium--deploy-qa', 'jenkins@example.com:/var/www/versions/qa/', 'telus-thorium--deploy-dev') {
+  triggers {
+    upstream('telus-thorium--deploy-dev')
+  }
 }
 
 /**
  * telus-thorium--deploy-stage copies the static site contents to the Staging
  * web server. It takes those artifacts from its upstream job, which is QA.
  */
-createJenkinsDeployJob('telus-thorium--deploy-stage', 'jenkins@example.com:/var/www/versions/stage/', 'upstream') {
-    triggers {
-	upstream('telus-thorium--deploy-qa')
-    }
-}
+createJenkinsDeployJob('telus-thorium--deploy-stage', 'jenkins@example.com:/var/www/versions/stage/', 'telus-thorium--deploy-qa')
 
 /**
  * telus-thorium--deploy-prod copies the static site contents to the production
@@ -153,33 +157,94 @@ createJenkinsDeployJob('telus-thorium--deploy-prod', 'jenkins@example.com:/var/w
  */
 
 def createJenkinsJob (String name, Closure closure) {
-    job(name) {
-	wrappers {
-	    colorizeOutput()
-	}
-    }.with closure
+  job(name) {
+    wrappers {
+      colorizeOutput()
+    }
+    logRotator {
+      numToKeep(5)
+      artifactNumToKeep(1)
+    }
+    publishers {
+      /*
+       * TODO: add actual slack auth
+       */
+      slackNotifier {
+        room('#nextlevel-tds')
+        authToken('someAuthToken')
+        authTokenCredentialId('someCredentialId')
+        notifyFailure(true)
+        notifyAborted(false)
+        notifyNotBuilt(false)
+        notifyUnstable(false)
+        notifyBackToNormal(true)
+        notifySuccess(false)
+        notifyRepeatedFailure(false)
+        startNotification(false)
+        includeTestSummary(false)
+        includeCustomMessage(false)
+        customMessage(null)
+        sendAs(null)
+        commitInfoChoice('NONE')
+        teamDomain('telusdigital')
+      }
+    }
+  }.with closure
 }
 
 def createJenkinsDeployJob(String name, String target, String artifactsSource = 'upstream', Closure closure = {}) {
-    job(name) {
-	wrappers {
-	    colorizeOutput()
-	    sshAgent('thorium-deployment-key')
-	}
-	steps {
-	    copyArtifacts(artifactsSource) {
-		buildSelector {
-		    latestSuccessful(true)
-		}
-	    }
-	    shell("rsync --delete -acvz docs/dist/docs/ ${target}")
+  job(name) {
+    wrappers {
+      colorizeOutput()
+      /*
+       * TODO: configure actual credentials for SSH/rsync when instances are ready
+       */
+      sshAgent('thorium-deployment-key')
+    }
+    steps {
+      copyArtifacts(artifactsSource) {
+        buildSelector {
+          latestSuccessful(true)
+        }
+      }
+      /*
+       * TODO: configure actual credentials for SSH/rsync when instances are ready
+       */
+      shell("rsync --delete -acvz docs/dist/docs/ ${target}")
 	}
 
-	publishers {
-	    archiveArtifacts {
-		pattern('docs/dist/docs/**/*')
-		onlyIfSuccessful()
-	    }
-	}
-    }.with closure
+    publishers {
+      archiveArtifacts {
+        pattern('docs/dist/docs/**/*')
+        onlyIfSuccessful()
+      }
+      /*
+       * TODO: add actual slack auth
+       */
+      slackNotifier {
+        room('#nextlevel-tds')
+        authToken('someAuthToken')
+        authTokenCredentialId('someCredentialId')
+        notifyFailure(true)
+        notifyAborted(false)
+        notifyNotBuilt(false)
+        notifyUnstable(false)
+        notifyBackToNormal(true)
+        notifySuccess(false)
+        notifyRepeatedFailure(false)
+        startNotification(false)
+        includeTestSummary(false)
+        includeCustomMessage(false)
+        customMessage(null)
+        sendAs(null)
+        commitInfoChoice('NONE')
+        teamDomain('telusdigital')
+      }
+    }
+
+    logRotator {
+      numToKeep(5)
+      artifactNumToKeep(1)
+    }
+  }.with closure
 }
