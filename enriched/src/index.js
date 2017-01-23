67186a6461567f70e5b515d8ca87e17262a9aae4
Ian McGonigle <imcgonigle@huge.com>
2017-01-23 14:01:22 -0500

    Restoring Card component.

--- enriched/src/index.js
+++ enriched/src/index.js
@@ -2,6 +2,7 @@ import SelectorCounter from './components/SelectorCounter';
 import Icon from './components/Icon';
 import * as Grid from './components/Grid';
 import Collapsible from './components/Collapsible';
+import Card from './components/Card';
 import Notification from './components/Notification';
 
 export {
@@ -9,5 +10,6 @@ export {
   Icon,
   Grid,
   Collapsible,
+  Card,
   Notification,
 };
