13083cac1a514750e4939c60acf9223ad30a1955
Laura Cabrera <lzcabrera@gmail.com>
2017-05-10 11:03:28 -0700

    test(BlockDimpleDivider): update naming convention on unit test file

--- enriched/src/blocks/BlockDividerDimple/__tests__/BlockDividerDimple.spec.jsx
+++ enriched/src/blocks/BlockDividerDimple/__tests__/BlockDividerDimple.spec.jsx
@@ -2,16 +2,16 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import { shallowToJson } from 'enzyme-to-json';
 
-import DividerDimpleBlock from '../';
+import BlockDividerDimple from '../';
 
 describe('DividerDimple', () => {
   it('matches snapshot', () => {
-    const wrapper = shallow(<DividerDimpleBlock />);
+    const wrapper = shallow(<BlockDividerDimple />);
     expect(shallowToJson(wrapper)).toMatchSnapshot();
   });
 
   it('should use divider dimple style', () => {
-    const wrapper = shallow(<DividerDimpleBlock />);
+    const wrapper = shallow(<BlockDividerDimple />);
     expect(wrapper.hasClass('b-divider-dimple__bg')).toBeTruthy();
   });
 });
