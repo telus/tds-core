8ea00287b40798f4deb01c41495926816caddabe
Enrico Sacchetti <enrico.sacchetti@telus.com>
2017-07-20 15:08:22 -0400

    docs(card): add jsdoc

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -7,6 +7,9 @@ import { warn } from '../../deprecate';
 
 import './Card.scss';
 
+/**
+ * A card is a container that serves as an entry point to more detailed information. A card's width will match its parent.
+ */
 const Card = ({ className, children, ...rest }) => {
   if (className) {
     warn('Card', 'Custom CSS classes are deprecated. This component does not support custom styling.');
@@ -24,7 +27,13 @@ const Card = ({ className, children, ...rest }) => {
 };
 
 Card.propTypes = {
+  /**
+   * Pass a custom class.
+   */
   className: PropTypes.string,
+  /**
+   * Child elements.
+   */
   children: PropTypes.node.isRequired
 };
 
---
+++ src/components/Card/Card.md
@@ -0,0 +1,11 @@
+## Minimal usage
+
+```
+<Card>
+  <h2>Specifications</h2>
+  <ul>
+    <li>2.2gHz CPU</li>
+    <li>250GB Storage</li>
+  </ul>
+</Card>
+```
