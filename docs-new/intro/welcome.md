8b61d8198dc90cf789c5314b99623926ac2045b0
Ryan Oglesby <ryan.oglesby08@gmail.com>
2017-08-08 11:08:12 -0400

    ci(Deploy Docs): Edit deploy-docs script to deploy staging to a folder
    Because we have temporarily deployed the v0.18.1 prod docs to staging too.

--- docs-new/intro/welcome.md
+++ docs-new/intro/welcome.md
@@ -3,3 +3,5 @@ Welcome to the TELUS Design System (TDS).
 TDS is a set of living guidelines that communicates our brand promise through our digital experiences. It's a holistic
 platform that integrates documentation, guidelines, and design management that serves as single source for digital
 design guidelines, code patterns and UI elements.
+
+**The documentation for v0.18.1 is temporarily available [here](http://cdn.telus-thorium-doc-staging.s3-website-us-east-1.amazonaws.com/v0.18.1).**
--- scripts/deploy-v0.18.1-docs.js
+++ scripts/deploy-v0.18.1-docs.js
@@ -11,6 +11,10 @@
 //  s3-website: https://github.com/klaemo/s3-website
 
 
+
+// THIS SCRIPT DEPLOYS THE V0.18.1 VERSION OF THE DOCS TO A FOLDER IN STAGING. IT SHOULD BE REMOVED SOON.
+
+
 if (!process.env.AWS_ACCESS_KEY_ID || !process.env.AWS_SECRET_ACCESS_KEY) {
   console.error("'AWS_ACCESS_KEY_ID' and 'AWS_SECRET_ACCESS_KEY' must be available in the environment.");
   process.exit(1);
@@ -23,7 +27,7 @@ const readDirSync = require('fs').readdirSync;
 const deploy = require('s3-website').deploy;
 const AWS = require('aws-sdk');
 
-const uploadDir = resolvePath('styleguide/');
+const uploadDir = resolvePath('old-docs/');
 
 // Holding off on using the version for now. Will need DNS changes to properly get versioned folders in the bucket.
 // const version = require(resolvePath('package.json')).version;
@@ -32,7 +36,7 @@ const env = process.argv[2] || 'staging';
 const config = {
   region: 'us-east-1',
   domain: `cdn.telus-thorium-doc-${env}`,
-  prefix: env === 'staging' ? 'latest' : undefined,      // Deploy to a directory in staging for now until we get versioning sorted out
+  prefix: 'v0.18.1',
   uploadDir: uploadDir,
   lockConfig: true
 };
@@ -51,35 +55,41 @@ const deployToS3 = (onDeploy) => {
   });
 };
 
-const grantReadPermissions = (directory) => {
-  readDirSync(directory)
-    .filter((file) => file !== 'build')
-    .forEach((file) => {
-      const absoluteFilePath = resolvePath(directory, file);
-
-      putObjectAcl(relativePath(uploadDir, absoluteFilePath))
-    });
-};
-
 const putObjectAcl = (key) => {
   const params = {
-    Bucket: config.domain,
+    Bucket: 'cdn.telus-thorium-doc-staging',
     ACL: 'public-read',
-    Key: env === 'staging' ? `latest/${key}` : key
+    Key: `v0.18.1/${key}`
   };
 
   s3.putObjectAcl(params, (err, data) => {
-    if (err) {
-      throw err;
+    if( !err ) {
+      console.log(data);
     }
-
-    console.log(data);
   });
 };
 
 
 deployToS3(() => {
+
   // FIXME: There seems to be something wrong with the permissions when uploading via s3-website. Objects are not public readable by default.
-  grantReadPermissions(uploadDir);
-  grantReadPermissions(resolvePath(uploadDir, 'build'));
+  [
+    'old-docs/1-About', 'old-docs/2-Use-TDS', 'old-docs/3-Foundational-Elements', 'old-docs/4-Global Components', 'old-docs/5-Block Components', 'old-docs/6-Blocks',
+    'old-docs/assets/bundles', 'old-docs/assets/bundles/images', 'old-docs/assets/css',
+    'old-docs/assets/downloads',
+    'old-docs/assets/fonts', 'old-docs/assets/fonts/helvetica/etext-45-light', 'old-docs/assets/fonts/helvetica/etext-55-roman', 'old-docs/assets/fonts/helvetica/etext-65-medium',
+    'old-docs/assets/images',
+    'old-docs/assets/js',
+    'old-docs/assets/plugins',
+    'old-docs/assets/templates',
+    'old-docs/examples',
+  ].forEach((directory) => {
+    readDirSync(directory)
+      .forEach((file) => {
+        const absoluteFilePath = resolvePath(directory, file);
+
+        putObjectAcl(relativePath(resolvePath('old-docs'), absoluteFilePath))
+      });
+  });
+
 });
