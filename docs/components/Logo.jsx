4db3102d83e3289ad5d9baf6c97d0e60a1db3877
Ryan Oglesby <ryan.oglesby08@gmail.com>
2017-09-22 14:28:06 -0500

    docs(logo): Use the Text component for the TDS version text

--- docs/components/Logo.jsx
+++ docs/components/Logo.jsx
@@ -3,13 +3,15 @@ import React from 'react'
 import { version } from '../../package.json'
 import logo from './Logo.svg'
 
+import Text from '../../src/components/Typography/Text/Text'
+
 const Logo = () => (
   <div>
     <a href="/" title="TELUS Design System">
       <img src={logo} alt="TELUS Logo" />
     </a>
 
-    TDS v{version}
+    <Text>TDS v{version}</Text>
   </div>
 )
 
