eb09e04de270d3e34deeba04ecd672a1682c0392
mac <codedavinci.now@gmail.com>
2017-11-20 11:31:25 -0800

    refactor(searchComponent) Update component markup due to code review by using between & inset  prop

--- docs/components/TableOfContents/TableOfContentsRenderer.jsx
+++ docs/components/TableOfContents/TableOfContentsRenderer.jsx
@@ -4,26 +4,22 @@ import Box from '../../../src/components/Box/Box'
 import Input from '../../../src/components/Input/Input'
 
 const TableOfContentsRenderer = ({ children, searchTerm, onSearchTermChange }) => {
-
   return (
-    <Box>
-      <Box inset={3} below={2}>
-        <Input
-          value={searchTerm}
-          label="Filter by name"
-          onChange={e => onSearchTermChange(e.target.value)}
-        />
-      </Box>
-      {children}
+    <Box between={2} inset={2}>
+      <Input
+        value={searchTerm}
+        label="Filter by name"
+        onChange={e => onSearchTermChange(e.target.value)}
+      />
+      <div>{children}</div>
     </Box>
   )
 }
 
-
 TableOfContentsRenderer.propTypes = {
   children: PropTypes.object.isRequired,
   searchTerm: PropTypes.string.isRequired,
-  onSearchTermChange: PropTypes.func.isRequired
+  onSearchTermChange: PropTypes.func.isRequired,
 }
 
 export default TableOfContentsRenderer
