b586b20c073aa94bd4b6c22c022a85471baefeb8
Ryan Oglesby <ryan.oglesby08@gmail.com>
2017-12-28 15:53:08 -0600

    test: Unskip and fix old tests for Spinner and SelectorCounter

--- src/old-components/Spinner/__tests__/Spinner.spec.jsx
+++ src/old-components/Spinner/__tests__/Spinner.spec.jsx
@@ -2,36 +2,68 @@ import React from 'react'
 import { shallow, mount } from 'enzyme'
 import Spinner from '../Spinner'
 
-
 describe('<Spinner />', () => {
   describe('in container mode', () => {
     it('spinner is visible when spinning is true', () => {
-      const wrapper = shallow(
-        <Spinner tip="Loading..." spinning />
-      )
-      expect(wrapper.find('.spinner').first().hasClass('spinner--spinning')).toBeTruthy()
-      expect(wrapper.find('.spinner__tip').first().text()).toBe('Loading...')
+      const wrapper = shallow(<Spinner tip="Loading..." spinning />)
+      expect(
+        wrapper
+          .find('.spinner')
+          .first()
+          .hasClass('spinner--spinning')
+      ).toBeTruthy()
+      expect(
+        wrapper
+          .find('.spinner__tip')
+          .first()
+          .text()
+      ).toBe('Loading...')
       wrapper.setProps({
-        spinning: false
+        spinning: false,
       })
-      expect(wrapper.find('.spinner').first().hasClass('spinner--spinning')).toBeFalsy()
+      expect(
+        wrapper
+          .find('.spinner')
+          .first()
+          .hasClass('spinner--spinning')
+      ).toBeFalsy()
     })
   })
 
   describe('embedded mode', () => {
-    it.skip('spinner is visible when spinning is true', () => {
+    it('spinner is visible when spinning is true', () => {
       const wrapper = mount(
         <Spinner tip="Loading..." spinning wrapperClassName="test">
           <p>TEST</p>
         </Spinner>
       )
-      expect(wrapper.find('.spinner').first().hasClass('spinner--spinning')).toBeTruthy()
-      expect(wrapper.find('.spinner__tip').first().text()).toBe('Loading...')
-      expect(wrapper.find('.spinner-container').first().hasClass('spinner-container--loading')).toBeTruthy()
+      expect(
+        wrapper
+          .find('.spinner')
+          .first()
+          .hasClass('spinner--spinning')
+      ).toBeTruthy()
+      expect(
+        wrapper
+          .find('.spinner__tip')
+          .first()
+          .text()
+      ).toBe('Loading...')
+      expect(
+        wrapper
+          .find('.spinner-container')
+          .first()
+          .hasClass('spinner-container--loading')
+      ).toBeTruthy()
       wrapper.setProps({
-        spinning: false
+        spinning: false,
       })
-      expect(wrapper.find('.spinner-container > p').first().text()).toBe('TEST')
+      expect(
+        wrapper
+          .find('.spinner-container > p')
+          .first()
+          .text()
+      ).toBe('TEST')
     })
   })
 })
