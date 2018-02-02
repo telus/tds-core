444dd461d464f88e092a2c024b9ac3c7e8001690
aquirkles <32460479+aquirkles@users.noreply.github.com>
2018-02-02 18:52:00 -0500

    feat(expand-collapse): Enable conditional rendering of panels (#447)
    * feat(expand-collapse): Enable conditional rendering of panels
    
    Allows developers to use common react patterns (ternary operators or the use of &&) to conditionally
    render panels inside the expand-collapse component
    
    444
    
    * chore(expand-collapse): Address code-review feedback
    
    Use the panelId for the key, should be safe as this is required and unique in the expand. Streamline
    the test for conditional render and update tests to pass ids to avoide duplicate key warning.

--- src/components/ExpandCollapse/Panels.jsx
+++ src/components/ExpandCollapse/Panels.jsx
@@ -14,24 +14,28 @@ const Panels = ({ topDivider, isPanelOpen, togglePanel, children, ...rest }) => 
   <div {...safeRest(rest)} className={styles.base}>
     {topDivider && <HairlineDivider />}
 
-    {React.Children.map(children, panel => {
-      const { id, header, subtext, tertiaryText, disabled, onToggle } = panel.props
+    {React.Children
+      .toArray(children)
+      .filter(Boolean)
+      .map(panel => {
+        const { id, header, subtext, tertiaryText, disabled, onToggle } = panel.props
 
-      return (
-        <PanelWrapper
-          panelId={id}
-          panelHeader={header}
-          panelSubtext={subtext}
-          panelTertiaryText={tertiaryText}
-          panelOnToggle={onToggle}
-          panelDisabled={disabled}
-          open={isPanelOpen(id)}
-          onClick={() => togglePanel(id)}
-        >
-          {panel}
-        </PanelWrapper>
-      )
-    })}
+        return (
+          <PanelWrapper
+            key={id}
+            panelId={id}
+            panelHeader={header}
+            panelSubtext={subtext}
+            panelTertiaryText={tertiaryText}
+            panelOnToggle={onToggle}
+            panelDisabled={disabled}
+            open={isPanelOpen(id)}
+            onClick={() => togglePanel(id)}
+          >
+            {panel}
+          </PanelWrapper>
+        )
+      })}
   </div>
 )
 
