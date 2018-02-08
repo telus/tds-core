358b30789ab9d1ba2225e443bc72ce44aadd846c
Ryan Oglesby <ryan.oglesby08@gmail.com>
2018-02-08 13:58:37 -0500

    docs(typography): Remove Heading section and reorder typography components with headings first

--- config/styleguide.config.js
+++ config/styleguide.config.js
@@ -156,6 +156,12 @@ module.exports = {
           content: path.resolve('src/components/Typography/Typography.md'),
           components() {
             return [
+              path.resolve('src/components/Typography/DisplayHeading/DisplayHeading.jsx'),
+              path.resolve(
+                'src/components/Typography/DisplayHeading/DisplayHeadingSup/DisplayHeadingSup.jsx'
+              ),
+              path.resolve('src/components/Typography/Heading/Heading.jsx'),
+              path.resolve('src/components/Typography/Heading/HeadingSup/HeadingSup.jsx'),
               path.resolve('src/components/Typography/Paragraph/Paragraph.jsx'),
               path.resolve('src/components/Typography/Text/Text.jsx'),
               path.resolve('src/components/Typography/Text/TextSup/TextSup.jsx'),
@@ -163,21 +169,6 @@ module.exports = {
               path.resolve('src/components/Typography/Small/Small.jsx'),
             ]
           },
-          sections: [
-            {
-              name: 'Headings',
-              components() {
-                return [
-                  path.resolve('src/components/Typography/DisplayHeading/DisplayHeading.jsx'),
-                  path.resolve(
-                    'src/components/Typography/DisplayHeading/DisplayHeadingSup/DisplayHeadingSup.jsx'
-                  ),
-                  path.resolve('src/components/Typography/Heading/Heading.jsx'),
-                  path.resolve('src/components/Typography/Heading/HeadingSup/HeadingSup.jsx'),
-                ]
-              },
-            },
-          ],
         },
         {
           name: 'Icons',
