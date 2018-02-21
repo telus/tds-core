6f340a081979b158d8420599463740f07fd577f2
Laura Cabrera <lzcabrera@gmail.com>
2018-02-21 09:42:21 -0800

    chore(scaffold): update scaffolding script and add new templates that work with lerna setup

--- scripts/scaffolding/Component.jsx
+++ scripts/scaffolding/Component.jsx
@@ -1,20 +1,14 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-import safeRest from '../../utils/safeRest'
+import safeRest from '../../shared/utils/safeRest'
 
 import styles from './$COMPONENT$.modules.scss'
 
-const $COMPONENT$ = ({ ...rest }) => (
-  <div {...safeRest(rest)} />
-)
+const $COMPONENT$ = ({ ...rest }) => <div {...safeRest(rest)} />
 
-$COMPONENT$.propTypes = {
+$COMPONENT$.propTypes = {}
 
-}
-
-$COMPONENT$.defaultProps = {
-
-}
+$COMPONENT$.defaultProps = {}
 
 export default $COMPONENT$
---
+++ scripts/scaffolding/index.cjs.js
@@ -0,0 +1,4 @@
+require('./dist/index.css')
+const $COMPONENT$ = require('./dist/index.cjs')
+
+module.exports = $COMPONENT$
