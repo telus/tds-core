d3462a18bae6e4e2120e04516aacce0d22149827
marcod1419 <marcod1419@gmail.com>
2018-03-26 15:44:17 -0400

    docs(core-select): version bump

--- packages/Select/Select.jsx
+++ packages/Select/Select.jsx
@@ -15,7 +15,7 @@ import styles from './Select.modules.scss'
 import iconWrapperStyles from '../../shared/styles/IconWrapper.modules.scss'
 
 /**
- * @version 1.0.3
+ * @version 1.0.4
  */
 const Select = ({ options, placeholder, ...props }) => (
   <FormField {...props}>
