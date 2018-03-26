4c74dcbf1cecc6affabbe2fef069686b464323eb
marcod1419 <marcod1419@gmail.com>
2018-03-26 15:41:31 -0400

    docs(core-input-feedback): version bump

--- packages/InputFeedback/InputFeedback.jsx
+++ packages/InputFeedback/InputFeedback.jsx
@@ -8,7 +8,7 @@ import safeRest from '../../shared/utils/safeRest'
 import styles from './InputFeedback.modules.scss'
 
 /**
- * @version 1.0.0
+ * @version 1.0.1
  *
  * A feedback box commonly used with form fields.
  */
