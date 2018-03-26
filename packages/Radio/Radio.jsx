2a5f668a58760e31bf3399d8a72665939e0ff0a2
marcod1419 <marcod1419@gmail.com>
2018-03-26 15:42:47 -0400

    docs(core-radio): version bump

--- packages/Radio/Radio.jsx
+++ packages/Radio/Radio.jsx
@@ -6,7 +6,7 @@ import Choice from '../../shared/components/Choice/Choice'
 import styles from './Radio.modules.scss'
 
 /**
- * @version 1.0.0
+ * @version 1.0.2
  */
 const Radio = props => (
   <Choice {...props} type="radio" inputTypeStyles={styles}>
