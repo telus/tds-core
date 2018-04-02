5c39befa64eb59477f152e868c9f37575601ff5c
Ryan Oglesby <ryan.oglesby08@gmail.com>
2018-04-02 10:42:37 -0500

    chore(docs): read version for colours and css reset from package.json

---
+++ docs/components/custom/PackageVersion/PackageVersion.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import Paragraph from '../../../../packages/Paragraph/Paragraph'
+
+const PackageVersion = ({ version }) => <Paragraph>Version: {version}</Paragraph>
+
+PackageVersion.propTypes = {
+  version: PropTypes.string.isRequired,
+}
+
+export default PackageVersion
