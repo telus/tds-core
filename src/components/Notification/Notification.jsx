4b146897396c9ddedb4757d54543276b69bc9901
Ryan Oglesby <ryan.oglesby08@gmail.com>
2018-04-11 10:02:00 -0400

    fix(notification): make error text meet WCAG AAA compliance

--- src/components/Notification/Notification.jsx
+++ src/components/Notification/Notification.jsx
@@ -5,11 +5,9 @@ import safeRest from '../../utils/safeRest'
 
 import FlexGrid from '../../../packages/FlexGrid/FlexGrid'
 import DecorativeIcon from '../../../packages/DecorativeIcon/DecorativeIcon'
-import ColoredTextProvider from '../Typography/ColoredTextProvider/ColoredTextProvider'
 import Paragraph from '../Typography/Paragraph/Paragraph'
 import Box from '../../../packages/Box/Box'
 
-import messagingStyles from '../Messaging.modules.scss'
 import styles from './Notification.modules.scss'
 
 const iconByVariant = {
@@ -27,20 +25,6 @@ const isImportant = variant => variant === 'success' || variant === 'error'
 
 const renderIcon = icon => <DecorativeIcon symbol={icon.symbol} variant={icon.color} />
 
-const renderContent = (variant, children) => {
-  const content = <Paragraph bold={isImportant(variant)}>{children}</Paragraph>
-
-  if (variant === 'error') {
-    return (
-      <ColoredTextProvider colorClassName={messagingStyles.errorText}>
-        {content}
-      </ColoredTextProvider>
-    )
-  }
-
-  return content
-}
-
 /**
  * A banner that highlights important messages.
  */
@@ -51,7 +35,7 @@ const Notification = ({ variant, children, ...rest }) => (
         <Box inline between={3}>
           {isImportant(variant) ? renderIcon(iconByVariant[variant]) : undefined}
 
-          {renderContent(variant, children)}
+          <Paragraph bold={isImportant(variant)}>{children}</Paragraph>
         </Box>
       </FlexGrid.Col>
     </FlexGrid>
