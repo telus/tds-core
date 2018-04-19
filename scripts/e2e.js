32deafc1ab6aadf5a9b3e878e103f1d962ddf4b6
Enrico Sacchetti <esacchetti@gmail.com>
2018-04-19 16:19:31 -0400

    test(e2e): update all baseline images (#589)
    ... to accommodate for the new width of the catalogue
    
    * docs(core-css-reset): add tilde to import statement
    
    * test(e2e): update all baseline images
    
    - since we display documentation at full width when the sidebar is hidden, all baseline images would be affected, this commit updates them
    - allow users to pass -a to `yarn test:e2e` to test all packages regardless if they've been updated
    
    * revert(docs): "docs(core-css-reset): add tilde to import statement"
    
    This reverts commit 35aef1f4eade0d18028975aa48ba98d93ddc1ed5.

--- scripts/e2e.js
+++ scripts/e2e.js
@@ -3,15 +3,16 @@
 // Usage notes
 // Nightwatch CLI options http://nightwatchjs.org/guide#command-line-options
 // Pass -u to update baseline images while running e2e tests
+// Pass -a to test all packages regardless if they've been updated
 // Pass the name of the package you wish to test such as `@tds/core-link`
 
 const { spawnSync } = require('child_process')
 const parseArgs = require('minimist')
-
 const getUpdatedPackageNames = require('./utils/getUpdatedPackageNames')
 
-getUpdatedPackageNames(packageNames => {
-  const parsedArgs = parseArgs(process.argv.slice(2))
+const parsedArgs = parseArgs(process.argv.slice(2))
+
+const runE2e = packageNames => {
   const onlyCorePackages = packageNames.filter(name => name.startsWith('@tds/core-')).join(' ')
 
   const { status } = spawnSync(
@@ -29,4 +30,6 @@ getUpdatedPackageNames(packageNames => {
   if (status !== 0) {
     process.exit(status)
   }
-})
+}
+
+getUpdatedPackageNames(packageNames => runE2e(packageNames), parsedArgs.a ? 'ls' : 'updated')
