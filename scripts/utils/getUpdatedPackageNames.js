8ce212dd0a1d2364c98ba1ca55e998031173ea2f
Enrico Sacchetti <enrico.sacchetti@telus.com>
2018-04-19 15:23:22 -0400

    test(e2e): update all baseline images
    - since we display documentation at full width when the sidebar is hidden, all baseline images would be affected, this commit updates them
    - allow users to pass -a to `yarn test:e2e` to test all packages regardless if they've been updated

--- scripts/utils/getUpdatedPackageNames.js
+++ scripts/utils/getUpdatedPackageNames.js
@@ -3,22 +3,25 @@
 const { exec } = require('child_process')
 const { userPackageNames, lernaOptions } = require('./parseArgs')
 
-const getUpdatedPackageNames = callback => {
+const getUpdatedPackageNames = (callback, operation = 'updated') => {
   if (userPackageNames.length > 0) {
     callback(userPackageNames)
     return
   }
 
-  exec(`./node_modules/.bin/lerna updated --json ${lernaOptions.join(' ')}`, (error, stdout) => {
-    if (stdout === '') {
-      console.log('No components have been changed, nothing to do. Exiting.')
-    } else {
-      const updatedPackages = JSON.parse(stdout)
-      const packageNames = updatedPackages.map(packageObject => packageObject.name)
+  exec(
+    `./node_modules/.bin/lerna ${operation} --json ${lernaOptions.join(' ')}`,
+    (error, stdout) => {
+      if (stdout === '') {
+        console.log('No components have been changed, nothing to do. Exiting.')
+      } else {
+        const updatedPackages = JSON.parse(stdout)
+        const packageNames = updatedPackages.map(packageObject => packageObject.name)
 
-      callback(packageNames)
+        callback(packageNames)
+      }
     }
-  })
+  )
 }
 
 module.exports = getUpdatedPackageNames
