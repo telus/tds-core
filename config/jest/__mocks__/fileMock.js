819ae06e942afd8cc00134866458b497fd9865a2
Ryan Oglesby <ryan.oglesby08@gmail.com>
2018-04-30 16:24:49 -0400

    chore(build): clean up jest config
    ...based on learnings from tds-community

--- config/jest/__mocks__/fileMock.js
+++
@@ -1,4 +0,0 @@
-const imageSource =
-  'https://images.contentful.com/qqswi6c8cvkj/6x5TmicNocYcYMmWk2iKKk/0d61c8caf2460dbc59280e1493780570/Lifestyle_2Girls_RoofTop_Selfie_d.jpg?w=300&h=300'
-
-module.exports = { imageSource }
--- config/jest/__mocks__/requestAnimationFrame.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable */
-
-// From: https://reactjs.org/blog/2017/09/26/react-v16.0.html
-// "React also depends on requestAnimationFrame (even in test environments). A simple shim for test environments would be:"
-
-global.requestAnimationFrame = function(callback) {
-  setTimeout(callback, 0)
-}
