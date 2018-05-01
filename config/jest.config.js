cb5c0ef27659e803b3218519f20936bb3436ae87
Ryan Oglesby <ryan.oglesby08@gmail.com>
2018-05-01 11:02:28 -0400

    chore(build): clean up jest config (#612)
    ...based on learnings from tds-community

--- config/jest.config.js
+++ config/jest.config.js
@@ -6,14 +6,11 @@ module.exports = {
   moduleNameMapper: {
     '\\.modules.scss$': 'identity-obj-proxy',
     '^.+\\.(css|scss)$': path.resolve('config/jest/__mocks__/styleMock.js'),
-    '^.+\\.(svg)$': path.resolve('config/jest/__mocks__/fileMock.js'),
   },
   setupFiles: [
-    path.resolve('config/jest/__mocks__/requestAnimationFrame.js'),
     path.resolve('config/jest/setupEnzyme.js'),
     path.resolve('config/jest/setupGlobals.js'),
   ],
   setupTestFrameworkScriptFile: path.resolve('node_modules/jest-enzyme/lib/index.js'),
-  snapshotSerializers: ['enzyme-to-json/serializer'],
   testEnvironment: 'enzyme',
 }
