8d70c2134a8a1cb71f3143e82294f118a9dff7d9
Ani <anirudh.swaminathan@telus.com>
2018-05-08 15:36:38 -0400

    refactor(core-css-reset): cleans up font path and rollup entry

--- packages/css-reset/index.cjs.js
+++
@@ -1,4 +0,0 @@
-require('./dist/index.css')
-const fonts = require('./dist/index.cjs')
-
-module.exports = fonts
--- packages/css-reset/index.es.js
+++
@@ -1,4 +0,0 @@
-import './dist/index.css'
-import fonts from './dist/index.es'
-
-export default fonts
