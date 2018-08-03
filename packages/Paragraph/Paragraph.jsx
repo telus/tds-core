37b137850c51858787f1ea7757e5b3203a855687
Enrico Sacchetti <enrico.sacchetti@telus.com>
2018-08-03 11:09:31 -0400

    Revert "fix(core-paragraph): prevent overflow on ie11"
    This reverts commit 5ce482efc9686f69aef9844651c82f28d7b4f2b9.

--- packages/Paragraph/Paragraph.jsx
+++ packages/Paragraph/Paragraph.jsx
@@ -17,7 +17,6 @@ const Paragraph = ({ bold, size, align, invert, children, ...rest }, context) =>
   const paragraphColor = invert ? typographyStyles.invertedColor : typographyStyles.color
 
   const classes = joinClassNames(
-    styles.base,
     typographyStyles.wordBreak,
     spacingStyles.noSpacing,
     context.inheritColor ? typographyStyles.inheritColor : paragraphColor,
