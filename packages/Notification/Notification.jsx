b419a2c3e1cf6b46cb6ace35aa1d05309008a580
Jordan Raffoul <me@jordanraffoul.com>
2018-08-29 13:03:30 -0400

    feat(core-notification): add dismissible

--- packages/Notification/Notification.jsx
+++ packages/Notification/Notification.jsx
@@ -3,11 +3,14 @@ import PropTypes from 'prop-types'
 
 import FlexGrid from '@tds/core-flex-grid'
 import DecorativeIcon from '@tds/core-decorative-icon'
+import StandaloneIcon from '@tds/core-standalone-icon'
 import Paragraph from '@tds/core-paragraph'
 import Box from '@tds/core-box'
 
 import safeRest from '../../shared/utils/safeRest'
 import joinClassNames from '../../shared/utils/joinClassNames'
+import Fade from '../../shared/components/Animation/Fade'
+import Reveal from '../../shared/components/Animation/Reveal'
 
 import styles from './Notification.modules.scss'
 
@@ -31,32 +34,111 @@ const renderIcon = icon => <DecorativeIcon symbol={icon.symbol} variant={icon.co
  *
  * @version ./package.json
  */
-const Notification = ({ variant, children, ...rest }) => (
-  <Box {...safeRest(rest)} vertical={3} dangerouslyAddClassName={styles[variant]}>
-    <FlexGrid>
-      <FlexGrid.Row>
-        <FlexGrid.Col>
-          <Box inline between={3}>
-            {isImportant(variant) ? renderIcon(iconByVariant[variant]) : undefined}
-            <div
-              className={joinClassNames(
-                isImportant(variant) ? styles.withIcon : styles.withoutIcon
+class Notification extends React.Component {
+  state = {
+    dismissed: false,
+    contentWrapperHeight: undefined,
+  }
+
+  componentDidMount() {
+    if (this.props.dismissible) {
+      this.adjustContentHeight()
+    }
+  }
+
+  componentDidUpdate() {
+    if (this.props.dismissible) {
+      this.adjustContentHeight()
+    }
+  }
+
+  adjustContentHeight = () => {
+    if (this.contentWrapper.offsetHeight !== this.state.contentWrapperHeight) {
+      this.setState({ contentWrapperHeight: this.contentWrapper.offsetHeight })
+    }
+  }
+
+  renderNotification() {
+    const { variant, dismissible, dismissibleA11yLabel, children, ...rest } = this.props
+
+    return (
+      <Box
+        {...safeRest(rest)}
+        inset={3}
+        dangerouslyAddClassName={joinClassNames(styles.base, styles[variant])}
+      >
+        <FlexGrid>
+          <FlexGrid.Row>
+            <FlexGrid.Col xs={dismissible ? 11 : undefined}>
+              <Box inline between={3}>
+                {isImportant(variant) ? renderIcon(iconByVariant[variant]) : undefined}
+                <div
+                  className={joinClassNames(
+                    isImportant(variant) ? styles.withIcon : styles.withoutIcon
+                  )}
+                >
+                  <Paragraph bold={isImportant(variant)}>{children}</Paragraph>
+                </div>
+              </Box>
+            </FlexGrid.Col>
+          </FlexGrid.Row>
+        </FlexGrid>
+        {dismissible && (
+          <div className={styles.dismiss}>
+            <StandaloneIcon
+              symbol="times"
+              size={16}
+              a11yText={dismissibleA11yLabel}
+              variant={variant === 'instructional' ? 'secondary' : undefined}
+              onClick={() => this.setState(() => ({ dismissed: true }))}
+            />
+          </div>
+        )}
+      </Box>
+    )
+  }
+
+  render() {
+    if (this.props.dismissible) {
+      return (
+        <Reveal
+          timeout={400}
+          in={!this.state.dismissed}
+          height={this.state.contentWrapperHeight || 0}
+        >
+          {() => (
+            <Fade unmountOnExit timeout={500} in={!this.state.dismissed}>
+              {() => (
+                <div
+                  ref={c => {
+                    this.contentWrapper = c
+                  }}
+                >
+                  {this.renderNotification()}
+                </div>
               )}
-            >
-              <Paragraph bold={isImportant(variant)}>{children}</Paragraph>
-            </div>
-          </Box>
-        </FlexGrid.Col>
-      </FlexGrid.Row>
-    </FlexGrid>
-  </Box>
-)
+            </Fade>
+          )}
+        </Reveal>
+      )
+    }
+    return this.renderNotification()
+  }
+}
 
 Notification.propTypes = {
   /**
    * The appearance.
    */
   variant: PropTypes.oneOf(['instructional', 'branded', 'success', 'error']),
+  /**
+   * Whether or not to allow the Notificiation to be dismissed.
+   */
+  dismissible: PropTypes.bool,
+  /**
+   * A label for assistive technology
+   */
+  dismissibleA11yLabel: PropTypes.string,
   /**
    * The message. Can be raw text or text components.
    */
@@ -65,6 +147,8 @@ Notification.propTypes = {
 
 Notification.defaultProps = {
   variant: 'instructional',
+  dismissible: false,
+  dismissibleA11yLabel: undefined,
 }
 
 export default Notification
