427d90486cfe9398bcaf7dc467a28efd6e1c20ac
Marco Donnici <marcod1419@gmail.com>
2018-09-13 16:52:52 -0400

    feat(core-unordered-list): update styles, add sizing prop
    BREAKING CHANGE: add size prop to unordered list

--- packages/UnorderedList/UnorderedItem/UnorderedItem.jsx
+++ packages/UnorderedList/UnorderedItem/UnorderedItem.jsx
@@ -1,23 +1,28 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+import Text from '@tds/core-text'
+
 import safeRest from '../../../shared/utils/safeRest'
+import joinClassNames from '../../../shared/utils/joinClassNames'
 
 import styles from './UnorderedItem.modules.scss'
 
-const UnorderedItem = ({ listStyle, children, ...rest }) => (
-  <li {...safeRest(rest)} className={styles[listStyle]}>
-    {children}
+const UnorderedItem = ({ listStyle, size, children, ...rest }) => (
+  <li {...safeRest(rest)} className={joinClassNames(styles[listStyle], styles[size])}>
+    <Text size={size}>{children}</Text>
   </li>
 )
 
 UnorderedItem.propTypes = {
   listStyle: PropTypes.oneOf(['circle', 'checkmark', 'x']),
+  size: PropTypes.oneOf(['small', 'medium', 'large']),
   children: PropTypes.node.isRequired,
 }
 
 UnorderedItem.defaultProps = {
   listStyle: 'circle',
+  size: 'medium',
 }
 
 UnorderedItem.displayName = 'UnorderedList.Item'
--- packages/UnorderedList/UnorderedList.jsx
+++ packages/UnorderedList/UnorderedList.jsx
@@ -5,20 +5,26 @@ import { childrenOfType } from 'airbnb-prop-types'
 import Box from '@tds/core-box'
 
 import safeRest from '../../shared/utils/safeRest'
+import joinClassNames from '../../shared/utils/joinClassNames'
 
 import UnorderedItem from './UnorderedItem/UnorderedItem'
 
 import styles from '../../shared/styles/List.modules.scss'
 
-const injectListStyle = (child, listStyle) => React.cloneElement(child, { listStyle })
+const injectListStyle = (child, listStyle, size) => React.cloneElement(child, { listStyle, size })
 
 /**
  * @version ./package.json
  */
-const UnorderedList = ({ listStyle, children, ...rest }) => {
+const UnorderedList = ({ listStyle, size, children, ...rest }) => {
   return (
-    <Box {...safeRest(rest)} tag="ul" between={2} dangerouslyAddClassName={styles.base}>
-      {React.Children.map(children, child => injectListStyle(child, listStyle))}
+    <Box
+      {...safeRest(rest)}
+      tag="ul"
+      between={2}
+      dangerouslyAddClassName={joinClassNames(styles.base, styles[size])}
+    >
+      {React.Children.map(children, child => injectListStyle(child, listStyle, size))}
     </Box>
   )
 }
@@ -29,6 +35,10 @@ UnorderedList.propTypes = {
    */
   listStyle: PropTypes.oneOf(['circle', 'checkmark', 'x']),
   /**
+   * The size of the list's text.
+   */
+  size: PropTypes.oneOf(['small', 'medium', 'large']),
+  /**
    * The list items. Must be at least one `UnorderedList.Item`.
    */
   children: childrenOfType(UnorderedItem).isRequired,
@@ -36,6 +46,7 @@ UnorderedList.propTypes = {
 
 UnorderedList.defaultProps = {
   listStyle: 'circle',
+  size: 'medium',
 }
 
 UnorderedList.Item = UnorderedItem
