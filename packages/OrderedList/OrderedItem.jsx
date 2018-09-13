fe679c12c76091e9f98b6461b77d5b37560f42d3
Marco Donnici <marcod1419@gmail.com>
2018-09-13 17:00:52 -0400

    feat(core-ordered-list): update styles, add sizing prop
    BREAKING CHANGE: add size prop to ordered list

--- packages/OrderedList/OrderedItem.jsx
+++ packages/OrderedList/OrderedItem.jsx
@@ -1,12 +1,23 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+import Text from '@tds/core-text'
+
 import safeRest from '../../shared/utils/safeRest'
 
-const OrderedItem = ({ children, ...rest }) => <li {...safeRest(rest)}>{children}</li>
+const OrderedItem = ({ children, size, ...rest }) => (
+  <li {...safeRest(rest)}>
+    <Text size={size}>{children}</Text>
+  </li>
+)
 
 OrderedItem.propTypes = {
   children: PropTypes.node.isRequired,
+  size: PropTypes.oneOf(['small', 'medium', 'large']),
+}
+
+OrderedItem.defaultProps = {
+  size: 'medium',
 }
 
 OrderedItem.displayName = 'OrderedList.Item'
--- packages/OrderedList/OrderedList.jsx
+++ packages/OrderedList/OrderedList.jsx
@@ -5,25 +5,39 @@ import { childrenOfType } from 'airbnb-prop-types'
 import Box from '@tds/core-box'
 
 import safeRest from '../../shared/utils/safeRest'
+import joinClassNames from '../../shared/utils/joinClassNames'
 
 import OrderedItem from './OrderedItem'
 
 import styles from './OrderedList.modules.scss'
 
+const injectListStyle = (child, size) => React.cloneElement(child, { size })
+
 /**
  * @version ./package.json
  */
-const OrderedList = ({ listStyle, children, ...rest }) => (
-  <Box {...safeRest(rest)} tag="ol" between={2} dangerouslyAddClassName={styles[listStyle]}>
-    {children}
-  </Box>
-)
+const OrderedList = ({ listStyle, size, children, ...rest }) => {
+  return (
+    <Box
+      {...safeRest(rest)}
+      tag="ol"
+      between={2}
+      dangerouslyAddClassName={joinClassNames(styles[listStyle], styles[size])}
+    >
+      {React.Children.map(children, child => injectListStyle(child, size))}
+    </Box>
+  )
+}
 
 OrderedList.propTypes = {
   /**
    * The bullet style.
    */
   listStyle: PropTypes.oneOf(['decimal', 'upperAlpha', 'lowerAlpha']),
+  /**
+   * The size of the list's text.
+   */
+  size: PropTypes.oneOf(['small', 'medium', 'large']),
   /**
    * The list items. Must be at least one `OrderedList.Item`.
    */
@@ -32,6 +46,7 @@ OrderedList.propTypes = {
 
 OrderedList.defaultProps = {
   listStyle: 'decimal',
+  size: 'medium',
 }
 
 OrderedList.Item = OrderedItem
