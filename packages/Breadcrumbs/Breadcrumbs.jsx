8f603e02c9b251335e8ed4ce3ea9c6d284fab241
Jordan Raffoul <me@jordanraffoul.com>
2018-10-12 10:24:28 -0400

    feat(core-breadcrumbs): use proptype util

--- packages/Breadcrumbs/Breadcrumbs.jsx
+++ packages/Breadcrumbs/Breadcrumbs.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { componentWithName } from 'airbnb-prop-types'
+
+import { componentWithName } from '@tds/util-proptypes'
 
 import safeRest from '../../shared/utils/safeRest'
 
