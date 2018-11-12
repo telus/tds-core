f4b4be004a9525819862e12b101eec112b2af775
Marco Donnici <marcod1419@gmail.com>
2018-11-12 09:29:12 -0500

    refactor(core-button-group): add optional group label

--- packages/ButtonGroup/ButtonGroup.jsx
+++ packages/ButtonGroup/ButtonGroup.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 import Box from '@tds/core-box'
+import Text from '@tds/core-text'
 import { componentWithName } from '@tds/util-prop-types'
 
 import ButtonGroupItem from './ButtonGroupItem/ButtonGroupItem'
@@ -13,7 +14,7 @@ import styles from './ButtonGroup.modules.scss'
 /**
  * @version ./package.json
  */
-const ButtonGroup = ({ name, onChange, value, children, ...rest }) => {
+const ButtonGroup = ({ name, onChange, value, label, children, ...rest }) => {
   const getButtons = () => {
     return React.Children.map(children, child =>
       React.cloneElement(child, { name, onChange, selectedValue: value })
@@ -21,6 +22,13 @@ const ButtonGroup = ({ name, onChange, value, children, ...rest }) => {
   }
   return (
     <fieldset>
+      {label ? (
+        <legend>
+          <Text bold size="medium">
+            {label}
+          </Text>
+        </legend>
+      ) : null}
       <Box between={3} inline dangerouslyAddClassName={styles.buttonGroup} {...safeRest(rest)}>
         {getButtons()}
       </Box>
@@ -32,10 +40,12 @@ ButtonGroup.propTypes = {
   name: PropTypes.string.isRequired,
   onChange: PropTypes.func,
   value: PropTypes.string.isRequired,
+  label: PropTypes.string,
   children: componentWithName('ButtonGroupItem').isRequired,
 }
 
 ButtonGroup.defaultProps = {
+  label: undefined,
   onChange: undefined,
 }
 
