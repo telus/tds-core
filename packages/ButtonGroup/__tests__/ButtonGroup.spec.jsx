b8f7bc7a6efbed1f109dc074bfb8c4e60b4c6bb5
Marco Donnici <marcod1419@gmail.com>
2018-11-14 14:35:35 -0500

    test(core-button-group): add unit tests

--- packages/ButtonGroup/__tests__/ButtonGroup.spec.jsx
+++ packages/ButtonGroup/__tests__/ButtonGroup.spec.jsx
@@ -1,32 +1,84 @@
 import React from 'react'
-import { shallow } from 'enzyme'
+import { shallow, mount } from 'enzyme'
+
+import A11yContent from '@tds/core-a11y-content'
 
 import ButtonGroup from '../ButtonGroup'
 
 describe('ButtonGroup', () => {
-  const doShallow = (props = {}) => shallow(<ButtonGroup {...props} />)
+  const defaultProps = {
+    name: 'storageSize',
+    onChange: () => {
+      return true
+    },
+    value: '64gb',
+    label: 'Please select a storage size',
+  }
+  const doShallow = (props = {}) =>
+    shallow(
+      <ButtonGroup {...props}>
+        <ButtonGroup.Item value="64gb">64 GB</ButtonGroup.Item>
+        <ButtonGroup.Item value="128gb">128 GB</ButtonGroup.Item>
+        <ButtonGroup.Item value="256gb">256 GB</ButtonGroup.Item>
+      </ButtonGroup>
+    )
+
+  const doMount = (props = {}) =>
+    mount(
+      <ButtonGroup {...props}>
+        <ButtonGroup.Item value="64gb">64 GB</ButtonGroup.Item>
+        <ButtonGroup.Item value="128gb">128 GB</ButtonGroup.Item>
+        <ButtonGroup.Item value="256gb">256 GB</ButtonGroup.Item>
+      </ButtonGroup>
+    )
 
   it('renders', () => {
-    const buttonGroup = doShallow()
+    const buttonGroup = doShallow(defaultProps)
 
     expect(buttonGroup).toMatchSnapshot()
   })
 
-  it('does other things', () => {
-    const buttonGroup = doShallow()
+  it('selects the correct button', () => {
+    const buttonGroup = doMount(defaultProps)
+    expect(
+      buttonGroup
+        .children()
+        .find('input')
+        .get(0).props.checked
+    ).toEqual(true)
+  })
 
-    expect(buttonGroup).toExist()
+  it('renders a group label', () => {
+    const buttonGroup = doShallow(defaultProps)
+    expect(buttonGroup.find('legend')).toExist()
   })
 
   it('passes additional attributes to the element', () => {
-    const buttonGroup = doShallow({ id: 'the-id', 'data-some-attr': 'some value' })
+    const buttonGroup = doShallow({ defaultProps, id: 'the-id', 'data-some-attr': 'some value' })
 
     expect(buttonGroup).toHaveProp('id', 'the-id')
     expect(buttonGroup).toHaveProp('data-some-attr', 'some value')
   })
 
+  describe('Accepts A11yContent', () => {
+    it('connects to ButtonGroup.Item', () => {
+      const buttonGroup = mount(
+        <ButtonGroup {...defaultProps}>
+          <ButtonGroup.Item value="64gb">64 GB</ButtonGroup.Item>
+          <ButtonGroup.Item value="512gb">
+            512 GB
+            <A11yContent>test</A11yContent>
+          </ButtonGroup.Item>
+        </ButtonGroup>
+      )
+
+      expect(buttonGroup).toContainReact(<A11yContent>test</A11yContent>)
+    })
+  })
+
   it('does not allow custom CSS', () => {
     const buttonGroup = doShallow({
+      defaultProps,
       className: 'my-custom-class',
       style: { color: 'hotpink' },
     })
