3e2af5330576c892d73a0892d88f4868550373c7
Jordan Raffoul <me@jordanraffoul.com>
2018-12-20 13:41:46 -0500

    chore(deps): upgrade babel-loader to v8

--- config/rollup.config.js
+++ config/rollup.config.js
@@ -51,8 +51,9 @@ export default opts => {
           },
         }),
       babel({
-        plugins: ['external-helpers'],
+        runtimeHelpers: true,
         exclude: '../../node_modules/**',
+        configFile: '../../babel.config.js',
       }),
     ],
   }
