7bf1de7137ff16ac281e9382105e3bbd9cc04825
Enrico Sacchetti <enrico.sacchetti@telus.com>
2018-12-21 14:56:32 -0500

    chore(deps): restore babel dependencies
    adjust formatting in babel config

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,6 @@
+/* eslint-disable func-names */
 module.exports = function(api) {
-  api.cache(true);
+  api.cache(true)
   return {
     presets: ['@babel/preset-env', '@babel/preset-react'],
     plugins: ['@babel/plugin-proposal-class-properties'],
