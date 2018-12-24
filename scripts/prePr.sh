f6bc2eeac60db03785a588066b47004583c9c1e5
Marco Donnici <marcod1419@gmail.com>
2018-12-24 16:00:45 -0500

    chore(scripts): improve prePr error handling and output

--- scripts/prePr.sh
+++ scripts/prePr.sh
@@ -10,7 +10,9 @@ log_user 0
 spawn npx lerna version --conventional-commits
 log_user 1
 
-expect "Are you sure you want to create these versions?"
-send "n\r"
+set PROMPT [ expect "Are you sure you want to create these versions?"]
+if { $PROMPT != "" } {
+  send "n\r"
+}
 
-spawn echo "\nIf this is not what you expected, ensure that your commit messages follow the Conventional Commits specification 'https://conventionalcommits.org' and try again.`"
+puts "\nIf this is not what you expected, ensure that your commit messages follow the Conventional Commits specification 'https://conventionalcommits.org' and try again.`"
