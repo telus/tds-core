e071fe59c75ebdd61432e5cf3ca80d71a11333ed
Marco Donnici <marcod1419@gmail.com>
2018-12-27 11:18:27 -0500

    chore(scripts): update tds bot for new prepr script

--- scripts/travis/github-pr.js
+++ scripts/travis/github-pr.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+/* eslint-disable no-console */
 
 // Environment: These variables must be available in the environment.
 // GITHUB_TOKEN
@@ -8,27 +9,32 @@
 const request = require('request')
 const { spawnSync } = require('child_process')
 
-
-if (!process.env.GITHUB_TOKEN || !process.env.TRAVIS_REPO_SLUG || !process.env.TRAVIS_PULL_REQUEST) {
+if (
+  !process.env.GITHUB_TOKEN ||
+  !process.env.TRAVIS_REPO_SLUG ||
+  !process.env.TRAVIS_PULL_REQUEST
+) {
   console.error(
     "'GITHUB_TOKEN', 'TRAVIS_REPO_SLUG'., and 'TRAVIS_PULL_REQUEST' must be available in the environment."
   )
   process.exit(1)
 }
 
-const { output } = spawnSync('node', ['scripts/prePr.js', '--conventional-commits', '--yes'])
+const { output } = spawnSync('scripts/prePr.sh')
 const changes = output[1].toString('utf8')
 
 request.post({
-  url: `https://api.github.com/repos/${process.env.TRAVIS_REPO_SLUG}/issues/${process.env.TRAVIS_PULL_REQUEST}/comments`,
+  url: `https://api.github.com/repos/${process.env.TRAVIS_REPO_SLUG}/issues/${
+    process.env.TRAVIS_PULL_REQUEST
+  }/comments`,
   headers: {
     'User-Agent': 'TDS',
-    Authorization: `token ${process.env.GITHUB_TOKEN}`
+    Authorization: `token ${process.env.GITHUB_TOKEN}`,
   },
   json: true,
   body: {
     body: `\`\`\`
 ${changes.trim()}
-\`\`\``
-  }
+\`\`\``,
+  },
 })
