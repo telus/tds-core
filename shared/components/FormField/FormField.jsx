01a81722622f37c7528f0bccddfc871a8188d2ed
Jordan Raffoul <me@jordanraffoul.com>
2018-12-28 15:41:24 -0500

    fix(shared-form-field): replace airbnb proptypes with util-prop-types

--- shared/components/FormField/FormField.jsx
+++ shared/components/FormField/FormField.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { componentWithName } from 'airbnb-prop-types'
+import { componentWithName } from '@tds/util-prop-types'
 
 import Box from '@tds/core-box'
 import Text from '@tds/core-text'
