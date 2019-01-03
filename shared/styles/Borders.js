a25c416e9ea442aed0395868c87a6458c7b4ccab
Marco Donnici <marcod1419@gmail.com>
2019-01-03 16:32:29 -0500

    chore(shared): update borders, forms and typography for emotion

---
+++ shared/styles/Borders.js
@@ -0,0 +1,18 @@
+export const borders = {
+  thin: {
+    borderWidth: 1,
+    borderStyle: 'solid',
+  },
+
+  none: {
+    borderWidth: 0,
+  },
+
+  rounded: {
+    borderRadius: 4,
+  },
+
+  circular: {
+    borderRadius: '50%',
+  },
+}
---
+++ shared/styles/Forms.js
@@ -0,0 +1,6 @@
+import { typography } from './Typography/typography'
+
+export const forms = {
+  height: { height: '3.25rem' },
+  font: { fontFamily: typography.fontTelus },
+}
