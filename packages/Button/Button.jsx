57fca1b8c3d5f66d1ba6d1c8b9570103ee52db7a
Jordan Raffoul <me@jordanraffoul.com>
2019-01-07 09:53:57 -0500

    feat(core-button): upgrade to use styled-components

--- packages/Button/Button.jsx
+++ packages/Button/Button.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
+import styled from 'styled-components'
 import PropTypes from 'prop-types'
 import { componentWithName, or } from '@tds/util-prop-types'
 
 import { warn } from '../../shared/utils/warn'
 import safeRest from '../../shared/utils/safeRest'
-
-import BaseButton from '../../shared/components/BaseButton/BaseButton'
+import media from '../../shared/utils/media/media'
 
 const preventDisabling = ({ disabled, ...props }) => {
   if (disabled) {
@@ -15,17 +15,70 @@ const preventDisabling = ({ disabled, ...props }) => {
   return props
 }
 
+const BaseButton = styled.button`
+  border-width: 0;
+  border-radius: 4px;
+
+  height: 3.25rem;
+  margin: 0;
+  padding: 0 2rem;
+
+  cursor: pointer;
+
+  background: none;
+  transition: background 0.2s;
+
+  display: flex;
+  width: 100%;
+  align-items: center;
+  justify-content: center;
+
+  word-wrap: break-word;
+
+  font-size: 1rem;
+  letter-spacing: -0.8px;
+  line-height: 1.5;
+  font-weight: 700;
+
+
+  ${media.greaterThan('sm')`
+    display: inline-flex;
+
+    width: auto;
+    min-width: 180px;
+  `}
+`
+
+const ButtonPrimary = styled(BaseButton)`
+  background-color: #248700;
+  color: #FFFFFF;
+`
+
+const ButtonSecondary = styled(BaseButton)`
+  background-color: #4b286d;
+  color: #FFFFFF;
+`
+
+const ButtonInverted = styled(BaseButton)`
+  background-color: #FFFFFF;
+  color: #2a2c2e;
+`
+
 /**
  * @version ./package.json
  */
-const Button = ({ type, variant, children, ...rest }) => {
+const Button = ({ variant, dangerouslyAddClassName, ...rest }) => {
   const restNoDisabled = preventDisabling(rest)
 
-  return (
-    <BaseButton {...safeRest(restNoDisabled)} element="button" variant={variant} type={type}>
-      {children}
-    </BaseButton>
-  )
+  let ButtonComponent
+  if (variant === 'secondary') {
+    ButtonComponent = ButtonSecondary
+  } else if (variant === 'inverted') {
+    ButtonComponent = ButtonInverted
+  } else {
+    ButtonComponent = ButtonPrimary
+  }
+  return <ButtonComponent {...safeRest(restNoDisabled)} className={dangerouslyAddClassName} />
 }
 
 Button.propTypes = {
@@ -47,4 +100,5 @@ Button.defaultProps = {
   variant: 'primary',
 }
 
+/** @component */
 export default Button
