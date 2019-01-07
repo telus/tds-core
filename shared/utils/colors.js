e06619d2798424708c8849929329de10f1606310
Jordan Raffoul <me@jordanraffoul.com>
2019-01-07 12:11:45 -0500

    feat(shared): add color util

---
+++ shared/utils/colors.js
@@ -0,0 +1,26 @@
+const colors = {
+  accessibleGreen: '#248700',
+  telusGreen: '#6c0',
+  panache: '#f4f9f2',
+  shuttleGrey: '#54595f',
+  shark: '#2a2c2e',
+  athensGrey: '#f7f7f8',
+  telusPurple: '#4b286d',
+  whiteLilac: '#f2eff4',
+  cardinal: '#c12335',
+  lavendarBlush: '#fff6f8',
+  white: '#fff',
+  gainsboro: '#d8d8d8',
+}
+
+colors.typography = {
+  link: colors.shuttleGrey,
+  text: colors.shark,
+}
+
+colors.tokens = {
+  primary: colors.accessibleGreen,
+  secondary: colors.telusPurple,
+}
+
+export default colors
