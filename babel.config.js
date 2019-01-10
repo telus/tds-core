943e0a7e3509f3cd9fc38f322f5584259975ad5d
Marco Donnici <marcod1419@gmail.com>
2019-01-10 15:56:24 -0500

    chore(other): fix babel config for emotion

--- babel.config.js
+++ babel.config.js
@@ -3,11 +3,11 @@ module.exports = function(api) {
   api.cache(true)
   return {
     presets: ['@babel/preset-env', '@babel/preset-react'],
-    plugins: ['@babel/plugin-proposal-class-properties'],
+    plugins: ['@babel/plugin-proposal-class-properties', 'emotion'],
     env: {
       test: {
         presets: ['@babel/preset-env', '@babel/preset-react'],
-        plugins: ['@babel/plugin-proposal-class-properties'],
+        plugins: ['@babel/plugin-proposal-class-properties', ['emotion', { "sourceMap": true }]],
       },
     },
   }
