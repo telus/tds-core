24044c673bc2c8e123e03eb125b802cb582dbbd0
Ani <anirudh.swaminathan@telus.com>
2019-01-19 05:35:25 -0500

    ci(scripts): circle ci - updated babel config

--- babel.config.js
+++ babel.config.js
@@ -6,11 +6,11 @@ module.exports = function(api) {
     plugins: ['@babel/plugin-proposal-class-properties'],
     env: {
       test: {
-        presets: ['@babel/preset-env', '@babel/preset-react'],
+        presets: ['@babel/preset-env', { modules: false }, '@babel/preset-react'],
         plugins: ['@babel/plugin-proposal-class-properties'],
       },
       'dev:e2e': {
-        presets: ['@babel/preset-env', { modules: 'commonjs' }, '@babel/preset-react'],
+        presets: ['@babel/preset-env', { modules: false }, '@babel/preset-react'],
         plugins: ['@babel/plugin-proposal-class-properties'],
       },
     },
