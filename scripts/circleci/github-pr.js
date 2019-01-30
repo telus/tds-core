c9dbd4757db4b3891d0830654f2644a010f07416
Marco Donnici <marcod1419@gmail.com>
2019-01-30 10:41:13 -0500

    chore(other): switch to seperate prepr step

--- scripts/circleci/github-pr.js
+++ scripts/circleci/github-pr.js
@@ -11,11 +11,11 @@ const { spawnSync } = require('child_process')
 
 if (
   !process.env.GITHUB_TOKEN ||
-  !process.env.CIRCLE_PR_REPONAME ||
+  !process.env.CIRCLE_PROJECT_REPONAME ||
   !process.env.CIRCLE_PULL_REQUEST
 ) {
   console.error(
-    "'GITHUB_TOKEN', 'CIRCLE_PR_REPONAME'., and 'CIRCLE_PULL_REQUEST' must be available in the environment."
+    "'GITHUB_TOKEN', 'CIRCLE_PROJECT_REPONAME'., and 'CIRCLE_PULL_REQUEST' must be available in the environment."
   )
   process.exit(1)
 }
@@ -27,10 +27,9 @@ const formattedVersions = (output[1].toString('utf8').match(/(\s-.*)*/g) || [])
   .replace(/\s-\s/g, '\n- ')
 
 const changes = `Packages pending updates:\n${formattedVersions}\n\nIf this is not what you expected, ensure that your commit messages follow the TDS commit types guide on this page: https://tds.telus.com/contributing/developer-guide.html and try again.`
-
 request.post({
-  url: `https://api.github.com/repos/${process.env.CIRCLE_PR_REPONAME}/issues/${
-    process.env.CIRCLE_PULL_REQUEST
+  url: `https://api.github.com/repos/telus/${process.env.CIRCLE_PROJECT_REPONAME}/issues/${
+    /[^/]*$/.exec(process.env.CIRCLE_PULL_REQUEST)[0]
   }/comments`,
   headers: {
     'User-Agent': 'TDS',
