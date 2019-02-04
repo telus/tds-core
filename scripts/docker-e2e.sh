b555905850923565ad1a986596e4863802b23dcc
Marco Donnici <marcod1419@gmail.com>
2019-02-04 14:50:38 -0500

    chore(scripts): prevent empty e2e arguments

--- scripts/docker-e2e.sh
+++ scripts/docker-e2e.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
 ## Used by package.json to pass arguments into the docker-compose up command.
-
-mode=$1 docker-compose up --abort-on-container-exit e2e
+mode="$@" docker-compose up --abort-on-container-exit e2e
