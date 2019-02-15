2deb0c43c797b3565b73b80071fe34a97f7aef0d
Enrico Sacchetti <enrico.sacchetti@telus.com>
2019-02-15 12:27:34 -0500

    chore(scripts): fix parseArgs.js to use lerna updated
    packages weren't being scanned based on lerna updated results, until now

--- scripts/utils/parseArgs.js
+++ scripts/utils/parseArgs.js
@@ -2,7 +2,7 @@ const parseArgs = require('minimist')
 
 const TDS_OPTIONS = ['-a', '--all', '-u', '--update-screenshots']
 
-let originalArgs = process.argv.slice(2)
+let originalArgs = process.argv.slice(2).filter(arg => arg !== '')
 
 if (originalArgs.length === 1) {
   originalArgs = originalArgs[0].split(' ')
