efba4692baf9b5d3ef372789ccb17cc7b78af265
Jordan Raffoul <me@jordanraffoul.com>
2019-02-18 14:36:54 -0500

    chore(core-box): remove css

--- packages/Box/index.cjs.js
+++ packages/Box/index.cjs.js
@@ -1,4 +1,3 @@
-require('./dist/index.css')
 const Box = require('./dist/index.cjs')
 
 module.exports = Box
--- packages/Box/index.es.js
+++ packages/Box/index.es.js
@@ -1,4 +1,3 @@
-import './dist/index.css'
 import Box from './dist/index.es'
 
 export default Box
