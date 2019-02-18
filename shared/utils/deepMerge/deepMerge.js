da014a1b85c0e95273971163547c5056af0d0c53
Jordan Raffoul <me@jordanraffoul.com>
2019-02-18 15:10:32 -0500

    chore(util-deep-merge): filter out nulls

--- shared/utils/deepMerge/deepMerge.js
+++ shared/utils/deepMerge/deepMerge.js
@@ -1,23 +1,25 @@
 /* eslint-disable no-param-reassign */
+const isObject = obj => obj && typeof obj === 'object'
+
 const deepMerge = (...objects) => {
-  const isObject = obj => obj && typeof obj === 'object'
+  return objects
+    .filter(obj => isObject(obj))
+    .reduce((prev, obj) => {
+      Object.keys(obj).forEach(key => {
+        const pVal = prev[key]
+        const oVal = obj[key]
 
-  return objects.reduce((prev, obj) => {
-    Object.keys(obj).forEach(key => {
-      const pVal = prev[key]
-      const oVal = obj[key]
+        if (Array.isArray(pVal) && Array.isArray(oVal)) {
+          prev[key] = pVal.concat(...oVal)
+        } else if (isObject(pVal) && isObject(oVal)) {
+          prev[key] = deepMerge(pVal, oVal)
+        } else {
+          prev[key] = oVal
+        }
+      })
 
-      if (Array.isArray(pVal) && Array.isArray(oVal)) {
-        prev[key] = pVal.concat(...oVal)
-      } else if (isObject(pVal) && isObject(oVal)) {
-        prev[key] = deepMerge(pVal, oVal)
-      } else {
-        prev[key] = oVal
-      }
-    })
-
-    return prev
-  }, {})
+      return prev
+    }, {})
 }
 
 export default deepMerge
