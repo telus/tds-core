8c1889b09c6bd4ce0721fd3870c7af8f4896ce10
Jordan Raffoul <me@jordanraffoul.com>
2019-02-22 12:14:28 -0500

    feat(core-colours): add named js exports for colours
    Deprecate SASS colour variables

---
+++ packages/colours/colours.js
@@ -0,0 +1,30 @@
+// colours
+export const colorAccessibleGreen = '#248700'
+export const colorTelusGreen = '#6c0'
+export const colorPanache = '#f4f9f2'
+
+export const colorShuttleGrey = '#54595f'
+export const colorShark = '#2a2c2e'
+export const colorAthensGrey = '#f7f7f8'
+
+export const colorTelusPurple = '#4b286d'
+
+export const colorWhiteLilac = '#f2eff4'
+export const colorCardinal = '#c12335'
+export const colorLavenderBlush = '#fff6f8'
+export const colorWhite = '#fff'
+export const colorGainsboro = '#d8d8d8'
+
+// typography
+export const colorLink = colorShuttleGrey
+export const colorText = colorShark
+
+// tokens
+export const colorPrimary = colorAccessibleGreen
+export const colorSecondary = colorTelusPurple
+
+// icons
+export const colorIconPrimary = colorPrimary
+export const colorIconSecondary = colorSecondary
+export const colorIconDisabled = colorShuttleGrey
+export const colorIconError = colorCardinal
