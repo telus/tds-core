af3926525cab607a8b161a8c64ee70244f92b8f6
Andrew Lam <andrew.lam@telus.com>
2019-02-28 16:23:52 -0500

    feat(shared-animation): move animation to public component

---
+++ packages/Animation/index.cjs.js
@@ -0,0 +1,9 @@
+const Fade = require('./dist/index.cjs').Fade
+const Reveal = require('./dist/index.cjs').Reveal
+const Translate = require('./dist/index.cjs').Translate
+
+module.exports = {
+  Fade,
+  Reveal,
+  Translate,
+}
