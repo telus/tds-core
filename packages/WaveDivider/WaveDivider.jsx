dd8cd06b23d8055c7d74e77990b07955f46451bd
Ani <anirudh.swaminathan@telus.com>
2019-03-09 16:48:53 -0500

    feat(core-wave-divider): convert to styled-components
    BREAKING CHANGE: convert to styled-components

--- packages/WaveDivider/WaveDivider.jsx
+++ packages/WaveDivider/WaveDivider.jsx
@@ -1,18 +1,39 @@
 import React from 'react'
+import styled from 'styled-components'
 
 import safeRest from '../../shared/utils/safeRest'
 
-import styles from './WaveDivider.modules.scss'
+const StyledWaveDivider = styled.svg({
+  maxWidth: '100%',
+})
+const PathNoFill = styled.path({
+  fill: 'none',
+})
+
+// This prop is called 'opacityValue' since 'opacity' is a reserved SVG attribute
+const StyledG = styled.g(props => ({
+  opacity: props.opacityValue,
+}))
+
+const StyledRect = styled.rect(props => ({
+  fill: `url(#TDS_WaveDivider_gradient-${props.gradient})`,
+}))
+
+const StyledPath = styled.path(props => ({
+  fill: `url(#TDS_WaveDivider_gradient-${props.gradient})`,
+  opacity: ['2', '4', '6'].includes(props.gradient) ? 0.5 : 1,
+  isolation: ['2', '4', '6'].includes(props.gradient) ? 'isolate' : 'unset',
+}))
 
 /**
  * Separate page content blocks.
  *
  * @version ./package.json
  */
+
 const WaveDivider = ({ ...rest }) => (
-  <svg
+  <StyledWaveDivider
     {...safeRest(rest)}
-    className={styles.wave}
     xmlns="http://www.w3.org/2000/svg"
     width="1202"
     height="226"
@@ -124,58 +145,55 @@ const WaveDivider = ({ ...rest }) => (
     </defs>
     <g>
       <g>
-        <path
-          className={styles.noFill}
-          d="M293.89 144.8c24.26 1.32 47.12 2.06 67.8 2.06 193 0 395.64-56 590.52-67C886.68 66.05 821.57 56.1 769.84 56.1 614.41 56.1 452.32 117.3 293.89 144.8Z"
-        />
-        <g className={styles.opacity3}>
-          <path
-            className={styles.gradient1}
+        <PathNoFill d="M293.89 144.8c24.26 1.32 47.12 2.06 67.8 2.06 193 0 395.64-56 590.52-67C886.68 66.05 821.57 56.1 769.84 56.1 614.41 56.1 452.32 117.3 293.89 144.8Z" />
+        <StyledG opacityValue="0.3">
+          <StyledPath
+            gradient="1"
             d="M1200 146c-57.54-18-153.12-46.11-247.79-66.1-194.87 11.06-397.57 67-590.52 67-20.69 0-43.55-.74-67.8-2.06-46 8-91.66 13.16-136.77 13.16-42.2 0-100.58-6.26-157.12-14.1V224H1200Z"
           />
-          <g className={styles.opacity2}>
+          <StyledG opacityValue="0.2">
             <path d="M1200 146c-57.54-18-153.12-46.11-247.79-66.1-194.87 11.06-397.57 67-590.52 67-20.69 0-43.55-.74-67.8-2.06-46 8-91.66 13.16-136.77 13.16-42.2 0-100.58-6.26-157.12-14.1V224H1200Z" />
-          </g>
-        </g>
-        <path
-          className={styles.gradient2}
+          </StyledG>
+        </StyledG>
+        <StyledPath
+          gradient="2"
           d="M769.84 56.1c51.74 0 116.84 10 182.38 23.79C973.58 78.68 994.85 78 1016 78c49.13 0 118.77 11.47 184 25.06V0H0V114.49c90.58 12.22 202.25 25.38 293.89 30.31C452.32 117.3 614.41 56.1 769.84 56.1Z"
         />
-        <g className={styles.opacity3}>
-          <path
-            className={styles.gradient3}
+        <StyledG opacityValue="0.3">
+          <StyledPath
+            gradient="3"
             d="M293.89 144.8C202.25 139.8 90.58 126.67 0 114.45V143.9C56.54 151.74 114.92 158 157.12 158 202.23 158 247.91 152.8 293.89 144.8Z"
           />
-          <g className={styles.opacity2}>
+          <StyledG opacityValue="0.2">
             <path d="M293.89 144.8C202.25 139.8 90.58 126.67 0 114.45V143.9C56.54 151.74 114.92 158 157.12 158 202.23 158 247.91 152.8 293.89 144.8Z" />
-          </g>
-        </g>
-        <path
-          className={styles.gradient4}
+          </StyledG>
+        </StyledG>
+        <StyledPath
+          gradient="4"
           d="M293.89 144.8C202.25 139.8 90.58 126.67 0 114.45V143.9C56.54 151.74 114.92 158 157.12 158 202.23 158 247.91 152.8 293.89 144.8Z"
         />
-        <g className={styles.opacity3}>
-          <path
-            className={styles.gradient5}
+        <StyledG opacityValue="0.3">
+          <StyledPath
+            gradient="5"
             d="M952.21 79.89c94.67 20 190.24 48.11 247.79 66.1V103.05C1134.77 89.47 1065.13 78 1016 78 994.85 78 973.58 78.68 952.21 79.89Z"
           />
-          <g className={styles.opacity2}>
+          <StyledG opacityValue="0.2">
             <path d="M952.21 79.89c94.67 20 190.24 48.11 247.79 66.1V103.05C1134.77 89.47 1065.13 78 1016 78 994.85 78 973.58 78.68 952.21 79.89Z" />
-          </g>
-        </g>
-        <path
-          className={styles.gradient6}
+          </StyledG>
+        </StyledG>
+        <StyledPath
+          gradient="6"
           d="M952.21 79.89c94.67 20 190.24 48.11 247.79 66.1V103.05C1134.77 89.47 1065.13 78 1016 78 994.85 78 973.58 78.68 952.21 79.89Z"
         />
-        <rect className={styles.gradient7} width="180" height="226" />
-        <rect className={styles.gradient8} x="1020" width="180" height="226" />
-        <rect className={styles.gradient9} y="154" width="1202" height="72" />
+        <StyledRect gradient="7" width="180" height="226" />
+        <StyledRect gradient="8" x="1020" width="180" height="226" />
+        <StyledRect gradient="9" y="154" width="1202" height="72" />
       </g>
     </g>
     <g>
-      <rect className={styles.gradient10} y="158.3" width="1201.56" height="67.5" />
-      <rect
-        className={styles.gradient11}
+      <StyledRect gradient="10" y="158.3" width="1201.56" height="67.5" />
+      <StyledRect
+        gradient="11"
         x="0.44"
         y="0.21"
         width="1201.56"
@@ -183,7 +201,7 @@ const WaveDivider = ({ ...rest }) => (
         transform="translate(1202.45 67.7) rotate(179.98)"
       />
     </g>
-  </svg>
+  </StyledWaveDivider>
 )
 
 export default WaveDivider
--- packages/WaveDivider/index.es.js
+++ packages/WaveDivider/index.es.js
@@ -1,4 +1,3 @@
-import './dist/index.css'
 import WaveDivider from './dist/index.es'
 
 export default WaveDivider
