6668049854d75051f896fffd25b5410d68dd44bf
Marco Donnici <marcod1419@gmail.com>
2019-03-21 13:22:40 -0400

    chore(other): add babel plugin for async

--- babel.config.js
+++ babel.config.js
@@ -3,15 +3,27 @@ module.exports = function(api) {
   api.cache(true)
   return {
     presets: ['@babel/preset-env', '@babel/preset-react'],
-    plugins: ['@babel/plugin-proposal-class-properties', 'babel-plugin-styled-components'],
+    plugins: [
+      '@babel/plugin-transform-runtime',
+      '@babel/plugin-proposal-class-properties',
+      'babel-plugin-styled-components',
+    ],
     env: {
       test: {
         presets: ['@babel/preset-env', '@babel/preset-react'],
-        plugins: ['@babel/plugin-proposal-class-properties', 'babel-plugin-styled-components'],
+        plugins: [
+          '@babel/plugin-transform-runtime',
+          '@babel/plugin-proposal-class-properties',
+          'babel-plugin-styled-components',
+        ],
       },
       e2e: {
         presets: ['@babel/preset-env', '@babel/preset-react'],
-        plugins: ['@babel/plugin-proposal-class-properties', 'babel-plugin-styled-components'],
+        plugins: [
+          '@babel/plugin-transform-runtime',
+          '@babel/plugin-proposal-class-properties',
+          'babel-plugin-styled-components',
+        ],
       },
     },
   }
