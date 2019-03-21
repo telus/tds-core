dfb8bca6b9910285e631fb40d4cb173d7d46a689
Marco Donnici <marcod1419@gmail.com>
2019-03-21 13:44:50 -0400

    chore(config): update styleguide config for youtube video

--- config/styleguide.config.js
+++ config/styleguide.config.js
@@ -266,6 +266,7 @@ module.exports = {
             return [
               path.resolve('packages/Card/Card.jsx'),
               path.resolve('packages/Image/Image.jsx'),
+              path.resolve('packages/YoutubeVideo/YoutubeVideo.jsx'),
               path.resolve('packages/A11yContent/A11yContent.jsx'),
             ]
           },
