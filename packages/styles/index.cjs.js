c3d3e5a1fdfd229bf248f1c94adad0c6d3ba956c
Jordan Raffoul <me@jordanraffoul.com>
2019-03-21 11:12:26 -0400

    fix(shared-styles): use module.exports instead of exports

--- packages/styles/index.cjs.js
+++ packages/styles/index.cjs.js
@@ -1,8 +1,3 @@
 const styles = require('./dist/index.cjs')
 
-exports.borders = styles.borders
-exports.iconWrapper = styles.iconWrapper
-exports.links = styles.links
-exports.messaging = styles.messaging
-exports.position = styles.position
-exports.spacing = styles.spacing
+module.exports = styles
