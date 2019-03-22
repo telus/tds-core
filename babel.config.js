ca30af9047050237e516bb16a84db1535a7b67ce
Marco Donnici <marcod1419@gmail.com>
2019-03-22 15:26:29 -0400

    chore(deps): add core-js for expand-collapse, temporarily revert babel

--- babel.config.js
+++ babel.config.js
@@ -3,27 +3,15 @@ module.exports = function(api) {
   api.cache(true)
   return {
     presets: ['@babel/preset-env', '@babel/preset-react'],
-    plugins: [
-      '@babel/plugin-transform-runtime',
-      '@babel/plugin-proposal-class-properties',
-      'babel-plugin-styled-components',
-    ],
+    plugins: ['@babel/plugin-proposal-class-properties', 'babel-plugin-styled-components'],
     env: {
       test: {
         presets: ['@babel/preset-env', '@babel/preset-react'],
-        plugins: [
-          '@babel/plugin-transform-runtime',
-          '@babel/plugin-proposal-class-properties',
-          'babel-plugin-styled-components',
-        ],
+        plugins: ['@babel/plugin-proposal-class-properties', 'babel-plugin-styled-components'],
       },
       e2e: {
         presets: ['@babel/preset-env', '@babel/preset-react'],
-        plugins: [
-          '@babel/plugin-transform-runtime',
-          '@babel/plugin-proposal-class-properties',
-          'babel-plugin-styled-components',
-        ],
+        plugins: ['@babel/plugin-proposal-class-properties', 'babel-plugin-styled-components'],
       },
     },
   }
