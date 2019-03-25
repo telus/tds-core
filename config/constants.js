7237cebb29476fc77bcc5447f200e8d5e73ae4cc
Enrico Sacchetti <enrico.sacchetti@telus.com>
2019-03-25 15:28:59 -0400

    chore(config): add e2e ignore list to constants

--- config/constants.js
+++ config/constants.js
@@ -1,6 +1,7 @@
 // Lists of TDS components to ignore during builds, tests, and publishes
 
-exports.ignoredPackages = ['@tds/core-selector-counter', '@tds/core-benefit']
+exports.ignoredPackages = ['@tds/core-selector-counter']
+exports.ignoredPackagesE2E = ['@tds/core-selector-counter', '@tds/core-benefit']
 exports.componentsWithExportsE2E = {
   Benefit: ['BenefitWithHeading', 'BenefitNoHeading'],
 }
