0509a0da8e3987c2952e5b0ed75b3103abac65d8
Enrico Sacchetti <enrico.sacchetti@telus.com>
2019-03-25 15:13:05 -0400

    chore(config): add benefitnoheading to docs
    ignore benefit from e2e tests for now

--- config/constants.js
+++ config/constants.js
@@ -1,3 +1,6 @@
 // Lists of TDS components to ignore during builds, tests, and publishes
 
-exports.ignoredPackages = ['@tds/core-selector-counter']
+exports.ignoredPackages = ['@tds/core-selector-counter', '@tds/core-benefit']
+exports.componentsWithExportsE2E = {
+  Benefit: ['BenefitWithHeading', 'BenefitNoHeading'],
+}
