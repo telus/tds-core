adbcc9fb18ffc38f2f6868412b177f41ee85a6eb
Marco Donnici <marcod1419@gmail.com>
2019-04-16 15:22:33 -0400

    chore(core-unordered-list): remove css requires

--- packages/UnorderedList/index.cjs.js
+++ packages/UnorderedList/index.cjs.js
@@ -1,4 +1,3 @@
-require('./dist/index.css')
 const UnorderedList = require('./dist/index.cjs')
 
 module.exports = UnorderedList
--- packages/UnorderedList/index.es.js
+++ packages/UnorderedList/index.es.js
@@ -1,4 +1,3 @@
-import './dist/index.css'
 import UnorderedList from './dist/index.es'
 
 export default UnorderedList
