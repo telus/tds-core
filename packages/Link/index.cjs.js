820b30b4c75d1c729811cda9dbeed2c9a32ef398
Marco Donnici <marcod1419@gmail.com>
2019-04-16 09:57:15 -0400

    chore(core-link): remove css require

--- packages/Link/index.cjs.js
+++ packages/Link/index.cjs.js
@@ -1,4 +1,3 @@
-require('./dist/index.css')
 const Link = require('./dist/index.cjs')
 
 module.exports = Link
--- packages/Link/index.es.js
+++ packages/Link/index.es.js
@@ -1,4 +1,3 @@
-import './dist/index.css'
 import Link from './dist/index.es'
 
 export default Link
