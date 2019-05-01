869b6eb0cb272ef3e9c553328fc65163a30c091c
Jordan Raffoul <me@jordanraffoul.com>
2019-05-01 15:30:42 -0400

    chore(core-terms-and-conditions): update sticky snapshots

--- packages/TermsAndConditions/Sticky/__tests__/Sticky.spec.jsx
+++ packages/TermsAndConditions/Sticky/__tests__/Sticky.spec.jsx
@@ -1,36 +1,105 @@
-import React from 'react'
-import { shallow, mount, render } from 'enzyme'
+import React, { useRef } from 'react'
+import { mount, render } from 'enzyme'
 
 import Sticky from '../Sticky'
 
 const defaultProps = {
-  content: 'This is content',
   number: 3,
-  onCloseClick: () => {},
+  content:
+    'Available for new activations and renewals on a 2-year term with any rate plan with a minimum monthly spend of $55/mo. TELUS reserves the right to modify prices and eligible rate plans with this offer at any time without advance notice.',
+  onClose: () => {},
 }
 
-describe('TermsAndConditions', () => {
-  // const doShallow = (props = {}) => shallow(<Sticky {...defaultProps} {...props} />)
-  // const doMount = (props = {}) => mount(<Sticky {...defaultProps} {...props} />)
-  // const doRender = (props = {}) => render(<Sticky {...defaultProps} {...props} />)
-  it('works', () => {
-    expect(true).toBe(true)
+const Wrapper = props => {
+  const returnRef = useRef(null)
+  return (
+    <div>
+      <button type="button" ref={returnRef}>
+        Button
+      </button>
+      <Sticky {...props} returnRef={returnRef} />
+    </div>
+  )
+}
+
+describe('Sticky', () => {
+  const doRenderWrapper = (props = {}) => render(<Wrapper {...defaultProps} {...props} />)
+  const doMountWrapper = (props = {}, options = {}) =>
+    mount(<Wrapper {...defaultProps} {...props} />, options)
+
+  it('renders closed', () => {
+    const sticky = doRenderWrapper()
+    expect(sticky).toMatchSnapshot()
   })
 
-  // describe('Sticky', () => {
-  //   it('renders an ordered list with a single item', () => {
-  //     const sticky = doMount({ number: 5 })
-  //     expect(sticky).toContainExactlyOneMatchingElement('ol')
-  //   })
+  it('renders opened', () => {
+    const sticky = doRenderWrapper({ isOpen: true })
+    expect(sticky).toMatchSnapshot()
+  })
 
-  //   it('renders an ordered list with the number', () => {
-  //     const sticky = doMount({ number: 5 })
-  //     expect(sticky.find('ol')).toHaveProp('start', 5)
-  //   })
+  describe('onClose', () => {
+    it('calls onClose when ESCAPE is pressed', () => {
+      const events = {}
+      window.addEventListener = jest.fn((event, cb) => {
+        events[event] = cb
+      })
 
-  //   it('renders an ordered list with an item with content', () => {
-  //     const sticky = doMount({ content: 'This is not the content' })
-  //     expect(sticky.find('li span')).toHaveProp('children', 'This is not the content')
-  //   })
-  // })
+      const onClose = jest.fn()
+      doMountWrapper({ isOpen: true, onClose })
+      events.keydown({ type: 'keydown', key: 'Escape' })
+      expect(onClose).toHaveBeenCalled()
+    })
+
+    it('calls onClose when clicking outside the Sticky', () => {
+      const events = {}
+      window.addEventListener = jest.fn((event, cb) => {
+        events[event] = cb
+      })
+
+      const onClose = jest.fn()
+      doMountWrapper({ isOpen: true, onClose })
+
+      events.mousedown({ type: 'mousedown', x: 0, y: 0 })
+      expect(onClose).toHaveBeenCalled()
+    })
+
+    it('calls onClose when clicking the close button', () => {
+      const events = {}
+      window.addEventListener = jest.fn((event, cb) => {
+        events[event] = cb
+      })
+
+      const onClose = jest.fn()
+      const sticky = doMountWrapper({ isOpen: true, onClose })
+
+      sticky
+        .find('Sticky')
+        .find('button')
+        .simulate('click')
+
+      expect(onClose).toHaveBeenCalled()
+    })
+
+    xit('focuses on the returnRef when closed', () => {
+      const events = {}
+      window.addEventListener = jest.fn((event, cb) => {
+        events[event] = cb
+      })
+
+      const onClose = jest.fn()
+      const sticky = doMountWrapper({ isOpen: true, onClose })
+
+      sticky
+        .find('Sticky')
+        .find('button')
+        .simulate('click')
+
+      expect(
+        sticky
+          .find('button')
+          .at(0)
+          .getDOMNode()
+      ).toEqual(document.activeElement)
+    })
+  })
 })
