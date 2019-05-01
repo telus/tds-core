e046a373b9df4385b64f5aff2576f4d1e7038baa
Jordan Raffoul <me@jordanraffoul.com>
2019-05-01 11:26:29 -0400

    feat(shared-animation): add FadeReveal component

--- packages/Animation/index.js
+++ packages/Animation/index.js
@@ -1,3 +1,4 @@
 export { default as Fade } from './Fade'
 export { default as Reveal } from './Reveal'
 export { default as Translate } from './Translate'
+export { default as FadeAndReveal } from './FadeAndReveal'
