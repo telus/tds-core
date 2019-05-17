1d244b33900d4ed84073fbe6273c1826d7633650
Jordan Raffoul <me@jordanraffoul.com>
2019-05-17 11:54:26 -0400

    feat(core-input): upgrade to styled-components
    BREAKING CHANGE: upgrade to styled-components

--- packages/Input/Input.jsx
+++ packages/Input/Input.jsx
@@ -1,40 +1,212 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
+import styled from 'styled-components'
+
 import { componentWithName } from '@tds/util-prop-types'
 
 import InputFeedback from '@tds/core-input-feedback'
+import Box from '@tds/core-box'
+import Text from '@tds/core-text'
+import Paragraph from '@tds/core-paragraph'
+import { borders, position, forms } from '@tds/shared-styles'
+import { FeedbackIcon } from '@tds/shared-form-field'
+import {
+  colorShuttleGrey,
+  colorWhite,
+  colorPrimary,
+  colorCardinal,
+  colorAthensGrey,
+} from '@tds/core-colours'
+import { medium, mediumFont, color } from '@tds/shared-typography'
 
-import FormField, { FeedbackIcon } from '@tds/shared-form-field'
-import joinClassNames from '../../shared/utils/joinClassNames'
+import safeRest from '../../shared/utils/safeRest'
+import generateId from '../../shared/utils/generateId/generateId'
 
-import positionStyles from '../../shared/styles/Position.modules.scss'
-import styles from './Input.modules.scss'
+const StyledLabelContainer = styled(Box)({
+  alignItems: 'center',
+})
+
+const StyledInput = styled.input(
+  {
+    width: '100%',
+    margin: 0,
+    outline: 0,
+    textOverflow: 'ellipsis',
+    borderColor: colorShuttleGrey,
+    '&::placeholder': {
+      font: 'inherit',
+      letterSpacing: 'inherit',
+      lineHeight: 'inherit',
+      color: colorShuttleGrey,
+    },
+  },
+  borders.thin,
+  borders.rounded,
+  forms.font,
+  medium,
+  mediumFont,
+  color,
+  ({ withFeedbackIcon }) => ({
+    '&::-webkit-inner-spin-button, &::-webkit-outer-spin-button': {
+      '-webkit-appearance:': 'none',
+      margin: 0,
+    },
+    minHeight: '3.25rem',
+    maxHeight: '3.25rem',
+    padding: withFeedbackIcon ? '0.5rem 3rem 0.5rem 1rem' : '0.5rem 1rem',
+  }),
+  {
+    '&:focus': {
+      borderColor: 'transparent',
+      boxShadow: `0 0 4px 1px ${colorShuttleGrey}`,
+      backgroundColor: colorWhite,
+    },
+  },
+  ({ feedback }) => {
+    let borderColor
+    if (feedback === 'success') {
+      borderColor = colorPrimary
+    } else if (feedback === 'error') {
+      borderColor = colorCardinal
+    }
+    return {
+      '&:not(:focus)': {
+        borderColor,
+      },
+    }
+  },
+  ({ disabled }) => {
+    if (disabled) {
+      return {
+        backgroundColor: colorAthensGrey,
+        borderColor: 'transparent',
+        '&:not(:focus)': {
+          borderColor: 'transparent',
+        },
+      }
+    }
+    return {}
+  }
+)
+
+const StyledFeedbackIcon = styled.div(
+  {
+    right: '1rem',
+  },
+  position.absolute,
+  position.centerVertically
+)
+
+const showFeedbackIcon = feedback => feedback === 'error' || feedback === 'success'
+const renderHint = (hint, Component, id) => (
+  <Component id={id} size="small">
+    {hint}
+  </Component>
+)
+const renderError = (error, errorId) => (
+  <InputFeedback id={errorId} feedback="error">
+    <Paragraph size="small">{error}</Paragraph>
+  </InputFeedback>
+)
+const renderLabel = (id, label, hint, hintPosition, hintId, tooltip) => (
+  <div>
+    <Box inline between="space-between">
+      <label htmlFor={id || generateId(label).identity()}>
+        <StyledLabelContainer inline tag="span" between={2}>
+          <Text size="medium" bold>
+            {label}
+          </Text>
+          {hint && hintPosition === 'inline' && renderHint(hint, Text, hintId)}
+        </StyledLabelContainer>
+      </label>
+      {tooltip && React.cloneElement(tooltip, { connectedFieldLabel: label })}
+    </Box>
+    {hint && hintPosition === 'below' && renderHint(hint, Paragraph, hintId)}
+  </div>
+)
+const renderHelper = (helper, helperId, feedback, value) => {
+  if (typeof helper === 'function') {
+    return (
+      <div id={helperId}>
+        <Text size="small">{helper(feedback, value)}</Text>
+      </div>
+    )
+  }
+
+  return (
+    <InputFeedback id={helperId} feedback={feedback}>
+      <Text size="small">{helper}</Text>
+    </InputFeedback>
+  )
+}
 
 /**
  * @version ./package.json
  */
-const Input = props => (
-  <FormField {...props}>
-    {({ className, ...inputProps }, showFeedbackIcon, feedback) => (
-      <div className={positionStyles.relative}>
-        <input
-          {...inputProps}
-          className={joinClassNames(
-            className,
-            showFeedbackIcon ? styles.withFeedbackIcon : styles.withoutFeedbackIcon
+const Input = React.forwardRef(
+  (
+    { id, value, type, label, hint, hintPosition, feedback, error, helper, tooltip, ...rest },
+    ref
+  ) => {
+    const [isFocused, setIsFocused] = useState(false)
+    const fieldId = generateId(id, rest.name, label)
+    const errorId = error && fieldId.postfix('error-message')
+    const helperId = helper && fieldId.postfix('helper')
+    const hintId = hint && hintPosition === 'below' && fieldId.postfix('hint')
+
+    const handleFocus = e => {
+      setIsFocused(true)
+      if (rest.onFocus) {
+        rest.onFocus(e)
+      }
+    }
+
+    const handleBlur = e => {
+      setIsFocused(false)
+      if (rest.onBlur) {
+        rest.onBlur(e)
+      }
+    }
+
+    return (
+      <Box between={2}>
+        {renderLabel(fieldId.identity(), label, hint, hintPosition, hintId, tooltip)}
+        {helper && renderHelper(helper, helperId, feedback, value)}
+        {error && renderError(error, errorId)}
+        <div css={{ position: 'relative' }}>
+          <StyledInput
+            {...safeRest(rest)}
+            type={type}
+            ref={ref}
+            id={fieldId.identity()}
+            value={value}
+            feedback={feedback}
+            aria-invalid={feedback === 'error'}
+            aria-describedby={errorId || hintId || helperId || undefined}
+            onFocus={handleFocus}
+            onBlur={handleBlur}
+          />
+          {!rest.disabled && (
+            <StyledFeedbackIcon>
+              <FeedbackIcon
+                showIcon={showFeedbackIcon(feedback) && !isFocused}
+                feedback={feedback}
+              />
+            </StyledFeedbackIcon>
           )}
-        />
-        {!inputProps.disabled && (
-          <div className={styles.iconsPosition}>
-            <FeedbackIcon showIcon={showFeedbackIcon} feedback={feedback} />
-          </div>
-        )}
-      </div>
-    )}
-  </FormField>
+        </div>
+      </Box>
+    )
+  }
 )
 
+Input.displayName = 'Input'
+
 Input.propTypes = {
+  /**
+   * The id.
+   */
+  id: PropTypes.string,
   /**
    * The HTML5 type of the input field.
    */
@@ -81,38 +253,18 @@ Input.propTypes = {
    * A `Tooltip`
    */
   tooltip: componentWithName('Tooltip'),
-  /**
-   * A callback function to be invoked when the input value changes.
-   *
-   * @param {SyntheticEvent} event The React `SyntheticEvent`
-   */
-  onChange: PropTypes.func,
-  /**
-   * A callback function to be invoked when the input receives focus.
-   *
-   * @param {SyntheticEvent} event The React `SyntheticEvent`
-   */
-  onFocus: PropTypes.func,
-  /**
-   * A callback function to be invoked when the input loses focus.
-   *
-   * @param {SyntheticEvent} event The React `SyntheticEvent`
-   */
-  onBlur: PropTypes.func,
 }
 
 Input.defaultProps = {
+  id: undefined,
   type: 'text',
   hint: undefined,
   hintPosition: 'inline',
   value: undefined,
   feedback: undefined,
   error: undefined,
-  helper: undefined,
   tooltip: undefined,
-  onChange: undefined,
-  onFocus: undefined,
-  onBlur: undefined,
+  helper: undefined,
 }
 
 export default Input
