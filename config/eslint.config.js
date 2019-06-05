f3ab5ec2717d6e572f5e8631fb86626e2e4f67f2
Jordan Raffoul <me@jordanraffoul.com>
2019-06-05 10:43:37 -0400

    chore(config): add react hook linter

--- config/eslint.config.js
+++ config/eslint.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   extends: ['@telus/eslint-config', 'plugin:prettier/recommended'], // Recommended eslint + prettier config: https://prettier.io/docs/en/eslint.html#why-not-both
   parser: 'babel-eslint',
+  plugins: ['react-hooks'],
   rules: {
     'react/require-default-props': 'warn',
     'import/no-extraneous-dependencies': false, // disabling it because all dev dependencies are pulled into the root
@@ -15,5 +16,7 @@ module.exports = {
     ],
     'react/destructuring-assignment': [true, 'always', { ignoreClassFields: false }],
     'react/jsx-one-expression-per-line': 0,
+    'react-hooks/rules-of-hooks': 'error', // Checks rules of Hooks
+    'react-hooks/exhaustive-deps': 'warn',
   },
 }
