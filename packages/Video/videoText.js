efc9d8967ce97d7998bd5324db0786ab2dde5f36
Marco Donnici <marcod1419@gmail.com>
2019-06-12 12:21:50 -0400

    fix(core-video): prevent page down when space is pressed
    - mute video on simpleMode
    - disable all keys except for play/pause on simpleMode

--- packages/Video/videoText.js
+++ packages/Video/videoText.js
@@ -3,6 +3,7 @@ const videoText = {
     videoPlayer:
       'Press the space bar or K to toggle play and pause. Use the left and right arrow keys or the comma and period keys to scrub through the video. Use the up and down arrow keys, the dash and equal keys, or the add and subtract number pad keys to increase and decrease volume. Press M to toggle mute. Press F to toggle fullscreen. Press escape to exit instructions and return to video.',
     videoSelected: 'Video selected. Press Q to listen to controls.',
+    videoSelectedSimple: 'Video selected. Press the space bar or K to toggle play and pause.',
     mute: 'Mute',
     unmute: 'Unmute',
     watch: 'Watch the video',
@@ -20,6 +21,7 @@ const videoText = {
     videoPlayer:
       'Appuyer sur K pour basculer entre lecture et pause. Utiliser les flèches gauche et droite ou les touches virgule et point pour reculer ou faire une avance rapide dans la vidéo. Utiliser les flèches ascendante et descendante, les touches tiret et égal ou les touches plus et moins du clavier numérique pour contrôler le volume. Appuyer sur la touche M pour désactiver le son. Appuyer sur F pour basculer en mode plein écran. Appuyer sur la touche d’échappement pour quitter les instructions et retourner à la vidéo.',
     videoSelected: 'Vidéo sélectionnée. Appuyer sur Q pour écouter les contrôles',
+    videoSelectedSimple: 'Vidéo sélectionnée. Appuyer sur K pour basculer entre lecture et pause.',
     mute: 'Désactiver le son',
     unmute: 'Activer le son',
     watch: 'Voir la vidéo',
