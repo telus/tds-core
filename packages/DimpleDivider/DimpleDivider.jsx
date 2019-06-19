8384c765f8eb55252da5c9e473e0f0ee303070d3
Jordan Raffoul <me@jordanraffoul.com>
2019-06-19 10:32:39 -0400

    feat(core-dimple-divider): upgrade to styled-components
    BREAKING CHANGE: add styled-components as a peerDependency

--- packages/DimpleDivider/DimpleDivider.jsx
+++ packages/DimpleDivider/DimpleDivider.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
+import styled from 'styled-components'
 
+import { borders, spacing } from '@tds/shared-styles'
 import safeRest from '../../shared/utils/safeRest'
 
-import styles from './DimpleDivider.modules.scss'
+const StyledDimpleDivider = styled.hr(spacing.noSpacing, borders.none, {
+  height: '2rem',
+  backgroundImage:
+    'radial-gradient(ellipse at top, rgba(150, 150, 150, 0.1) 0%, rgba(0, 0, 0, 0) 70%)',
+})
 
 /**
  * Separate modules.
  *
  * @version ./package.json
  */
-const DimpleDivider = ({ ...rest }) => <hr {...safeRest(rest)} className={styles.dimple} />
+const DimpleDivider = ({ ...rest }) => <StyledDimpleDivider {...safeRest(rest)} />
 
 export default DimpleDivider
