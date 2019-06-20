6d9cdcfc8f42bae7e8fa583c73f8e90b7dd9253e
Brendan Betts <brendan@powershifter.com>
2019-06-20 10:30:27 -0700

    fix(core-notification): renamed props to better reflect their purpose
    Rename onDismiss to onUnmount and onCloseClick to onDismiss. Update documentation to match changes.

--- packages/Notification/__tests__/Notification.spec.jsx
+++ packages/Notification/__tests__/Notification.spec.jsx
@@ -135,29 +135,6 @@ describe('<Notification />', () => {
       expect(icon.props().variant).toBeUndefined()
     })
   })
-  describe('onClickClose', () => {
-    it('calls the provided callback when dismissible', () => {
-      const onCloseClick = jest.fn()
-      const notification = doMount({
-        variant: 'error',
-        dismissible: true,
-        dismissibleA11yLabel: 'Close',
-        onCloseClick,
-      })
-      notification.find(StandaloneIcon).simulate('click')
-      expect(onCloseClick).toHaveBeenCalled()
-    })
-    it('does not call the provided callback when the dismissible prop is false', () => {
-      const onCloseClick = jest.fn()
-      const notification = doMount({
-        variant: 'error',
-        dismissible: false,
-        dismissibleA11yLabel: 'Close',
-        onCloseClick,
-      })
-      expect(notification.find(StandaloneIcon)).not.toExist()
-    })
-  })
   describe('onDismiss', () => {
     it('calls the provided callback when dismissible', () => {
       const onDismiss = jest.fn()
@@ -168,7 +145,6 @@ describe('<Notification />', () => {
         onDismiss,
       })
       notification.find(StandaloneIcon).simulate('click')
-      notification.unmount()
       expect(onDismiss).toHaveBeenCalled()
     })
     it('does not call the provided callback when the dismissible prop is false', () => {
@@ -179,9 +155,33 @@ describe('<Notification />', () => {
         dismissibleA11yLabel: 'Close',
         onDismiss,
       })
+      expect(notification.find(StandaloneIcon)).not.toExist()
+    })
+  })
+  describe('onUnmount', () => {
+    it('calls the provided callback when dismissible', () => {
+      const onUnmount = jest.fn()
+      const notification = doMount({
+        variant: 'error',
+        dismissible: true,
+        dismissibleA11yLabel: 'Close',
+        onUnmount,
+      })
+      notification.find(StandaloneIcon).simulate('click')
+      notification.unmount()
+      expect(onUnmount).toHaveBeenCalled()
+    })
+    it('does not call the provided callback when the dismissible prop is false', () => {
+      const onUnmount = jest.fn()
+      const notification = doMount({
+        variant: 'error',
+        dismissible: false,
+        dismissibleA11yLabel: 'Close',
+        onUnmount,
+      })
       notification.unmount()
       expect(notification.find(StandaloneIcon)).not.toExist()
-      expect(onDismiss).not.toHaveBeenCalled()
+      expect(onUnmount).not.toHaveBeenCalled()
     })
   })
 })
