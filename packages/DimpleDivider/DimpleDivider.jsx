6b8cd9a1ec276fe1903ec17e0826bedb33d1a6c1
Marco Donnici <marcod1419@gmail.com>
2019-06-21 10:49:07 -0400

    chore(core-dimple-divider): change height to px value

--- packages/DimpleDivider/DimpleDivider.jsx
+++ packages/DimpleDivider/DimpleDivider.jsx
@@ -5,7 +5,7 @@ import { borders, spacing } from '@tds/shared-styles'
 import safeRest from '../../shared/utils/safeRest'
 
 const StyledDimpleDivider = styled.hr(spacing.noSpacing, borders.none, {
-  height: '2rem',
+  height: '32px',
   backgroundImage:
     'radial-gradient(ellipse at top, rgba(150, 150, 150, 0.1) 0%, rgba(0, 0, 0, 0) 70%)',
 })
