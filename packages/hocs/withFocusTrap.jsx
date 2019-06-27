7544094cfc62d6d03b281a2f3f5cf440aa2dc6f7
Alessio Carnevale <alessio.carnevale@gmail.com>
2019-06-27 15:22:18 +0200

    feat(shared-hocs): adding aria attr. support - setting focus on render

--- packages/hocs/withFocusTrap.jsx
+++ packages/hocs/withFocusTrap.jsx
@@ -1,13 +1,24 @@
 /* eslint-disable jsx-a11y/no-noninteractive-tabindex */
-import React, { useRef } from 'react'
+import React, { useRef, useEffect } from 'react'
+import PropTypes from 'prop-types'
 
 // Returns an array of focusable elements in the order they are found in c
-const getFocusable = c =>
-  c.querySelectorAll('button, [href], input, select, textarea, [tabindex]:not([tabindex="-1"])')
+const selector =
+  'button, [href], input, select, textarea, [tabindex]:not([tabindex="-1"]), audio[controls], video[controls], [contenteditable]:not([contenteditable=false]'
+const getFocusable = c => c.querySelectorAll(selector)
 
 const withFocusTrap = Component => {
   const WithFocusTrap = props => {
     const componentRef = useRef(null)
+
+    useEffect(() => {
+      // setting the focus to the first focusable element
+      getFocusable(componentRef.current).focus()
+    })
+
+    // To force VoiceOver to treat the dialog as a modal we need to set the aria-label attribute.
+    // Also the modal-dialog html needs to be inserted into the page using JS after the page loads (this isn't a real problem)
+    const { ariaLabel, ...rest } = props
 
     const handleFocus = isEnd => () => {
       const focusableElements = getFocusable(componentRef.current)
@@ -15,13 +26,17 @@ const withFocusTrap = Component => {
     }
 
     return (
-      <div>
+      <div role="dialog" aria-modal="true" aria-label={ariaLabel}>
         <div onFocus={handleFocus(false)} tabIndex={0} />
-        <Component {...props} ref={componentRef} />
+        <Component {...rest} ref={componentRef} />
         <div onFocus={handleFocus(true)} tabIndex={0} />
       </div>
     )
   }
+  WithFocusTrap.propTypes = {
+    ariaLabel: PropTypes.string.isRequired,
+  }
+
   return WithFocusTrap
 }
 
