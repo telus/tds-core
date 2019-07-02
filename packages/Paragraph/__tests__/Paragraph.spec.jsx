d5715e78bb6306793c4d66b5f7547130c499b221
Jordan Raffoul <me@jordanraffoul.com>
2019-07-02 15:30:44 -0400

    feat(core-paragraph): upgrade to styled-components
    BREAKING CHANGE: upgrade to styled-components

--- packages/Paragraph/__tests__/Paragraph.spec.jsx
+++ packages/Paragraph/__tests__/Paragraph.spec.jsx
@@ -1,57 +1,66 @@
 import React from 'react'
-import { shallow } from 'enzyme'
+import { shallow, render } from 'enzyme'
 
-import Paragraph from '../Paragraph'
+import * as typography from '@tds/shared-typography'
+
+import Paragraph, { StyledParagraph } from '../Paragraph'
 
 describe('Paragraph', () => {
   const doShallow = props => shallow(<Paragraph {...props}>Some content</Paragraph>)
+  const doRender = props => render(<Paragraph {...props}>Some content</Paragraph>)
 
   it('renders', () => {
-    const paragraph = doShallow()
+    const paragraph = doRender()
 
     expect(paragraph).toMatchSnapshot()
   })
 
-  it('renders an HTML p tag', () => {
-    const paragraph = doShallow()
+  describe('StyledParagraph', () => {
+    const doShallowStyled = props =>
+      shallow(
+        <StyledParagraph size="medium" align="left" {...props}>
+          Some content
+        </StyledParagraph>
+      )
+    it('can be bold', () => {
+      let paragraph = doShallowStyled({ bold: false })
+      expect(paragraph).not.toHaveStyleRule('font-weight', '700')
+      expect(paragraph).toHaveStyleRule('font-weight', '400')
 
-    expect(paragraph).toHaveDisplayName('p')
-  })
+      paragraph = doShallowStyled({ bold: true })
+      expect(paragraph).toHaveStyleRule('font-weight', '700')
+      expect(paragraph).not.toHaveStyleRule('font-weight', '500')
+    })
 
-  it('can be bold', () => {
-    let paragraph = doShallow()
-    expect(paragraph).not.toHaveClassName('boldFont')
-    expect(paragraph).toHaveClassName('mediumFont')
+    it('can be inverted', () => {
+      let paragraph = doShallowStyled()
+      expect(paragraph).toHaveStyleRule('color', typography.color.color)
+      expect(paragraph).not.toHaveStyleRule('color', typography.invertedColor.color)
 
-    paragraph = doShallow({ bold: true })
-    expect(paragraph).toHaveClassName('boldFont')
-    expect(paragraph).not.toHaveClassName('mediumFont')
-  })
+      paragraph = doShallowStyled({ invert: true })
+      expect(paragraph).not.toHaveStyleRule('color', typography.color.color)
+      expect(paragraph).toHaveStyleRule('color', typography.invertedColor.color)
+    })
 
-  it('can be inverted', () => {
-    let paragraph = doShallow()
-    expect(paragraph).not.toHaveClassName('invertedColor')
-    expect(paragraph).toHaveClassName('color')
+    it('can be aligned', () => {
+      let paragraph = doShallowStyled()
+      expect(paragraph).toHaveStyleRule('text-align', 'left')
 
-    paragraph = doShallow({ invert: true })
-    expect(paragraph).toHaveClassName('invertedColor')
-    expect(paragraph).not.toHaveClassName('color')
-  })
+      paragraph = doShallowStyled({ align: 'right' })
+      expect(paragraph).toHaveStyleRule('text-align', 'right')
+    })
 
-  it('can be aligned', () => {
-    let paragraph = doShallow()
-    expect(paragraph).toHaveClassName('leftAlign')
+    it('can be sized', () => {
+      let paragraph = doShallowStyled()
+      expect(paragraph).toHaveStyleRule('font-size', '1rem')
+      expect(paragraph).toHaveStyleRule('letter-spacing', '-0.8px')
+      expect(paragraph).toHaveStyleRule('line-height', '1.5')
 
-    paragraph = doShallow({ align: 'right' })
-    expect(paragraph).toHaveClassName('rightAlign')
-  })
-
-  it('can be sized', () => {
-    let paragraph = doShallow()
-    expect(paragraph).toHaveClassName('medium mediumFont')
-
-    paragraph = doShallow({ size: 'small' })
-    expect(paragraph).toHaveClassName('small smallFont')
+      paragraph = doShallowStyled({ size: 'small' })
+      expect(paragraph).toHaveStyleRule('font-size', '0.875rem')
+      expect(paragraph).toHaveStyleRule('letter-spacing', '-0.6px')
+      expect(paragraph).toHaveStyleRule('line-height', '1.42857')
+    })
   })
 
   it('passes additional attributes to the p element', () => {
--- packages/Paragraph/index.es.js
+++ packages/Paragraph/index.es.js
@@ -1,4 +1,3 @@
-import './dist/index.css'
 import Paragraph from './dist/index.es'
 
 export default Paragraph
