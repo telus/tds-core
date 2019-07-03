431c3283d0447ff34a5c0ebacb4365c4b0c3b777
Jordan Raffoul <me@jordanraffoul.com>
2019-07-03 14:14:11 -0400

    feat(core-decorative-icon): upgrade to styled-components
    BREAKING CHANGE: upgrade to styled-components

--- packages/DecorativeIcon/SVGIcon.jsx
+++ packages/DecorativeIcon/SVGIcon.jsx
@@ -1,28 +1,40 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-import safeRest from '../../shared/utils/safeRest'
-import joinClassNames from '../../shared/utils/joinClassNames'
+import styled from 'styled-components'
+import { colorTelusPurple, colorShark, colorWhite } from '@tds/core-colours'
 
-import styles from '../../shared/components/Icon/Icon.modules.scss'
-import capitalize from '../../shared/utils/capitalize'
+import safeRest from '../../shared/utils/safeRest'
+
+const getColour = variant => {
+  switch (variant) {
+    case 'alternative':
+      return colorShark
+    case 'inverted':
+      return colorWhite
+    case 'default':
+    default:
+      return colorTelusPurple
+  }
+}
+
+const svgVariant = ({ variant }) => ({ '& > svg': { fill: getColour(variant) } })
+const svgSize = ({ size }) => ({ '& > svg': { width: size, height: size } })
+
+const StyledSVGIcon = styled.i(
+  {
+    display: 'inline-flex',
+  },
+  svgVariant,
+  svgSize
+)
 /**
  * SVG Icons are a type of DecorativeIcon that use SVGs instead of fonts to render the icons.
  *
  * @version ./package.json
  */
-const SVGIcon = ({ children, variant, size, ...rest }) => {
-  const classes = joinClassNames(
-    styles.svgBase,
-    variant && styles[`svgVariant${capitalize(variant)}`],
-    size && styles[`svgSize${size}`]
-  )
-
-  return (
-    <i {...safeRest(rest)} className={classes} aria-hidden="true">
-      {children}
-    </i>
-  )
+const SVGIcon = props => {
+  return <StyledSVGIcon {...safeRest(props)} aria-hidden="true" />
 }
 
 SVGIcon.propTypes = {
--- packages/DecorativeIcon/index.cjs.js
+++ packages/DecorativeIcon/index.cjs.js
@@ -1,4 +1,3 @@
-require('./dist/index.css')
 const DecorativeIcon = require('./dist/index.cjs')
 
 module.exports = DecorativeIcon
