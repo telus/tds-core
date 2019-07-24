258f3b8e17884f5431fe59645027aa0945bb318d
Marco Donnici <marcod1419@gmail.com>
2019-07-24 14:42:00 -0400

    feat(shared-styles): export seperate button and input heights

--- packages/styles/forms.js
+++ packages/styles/forms.js
@@ -1,7 +1,11 @@
 import { fontTelus } from '@tds/shared-typography'
 
-export const height = {
+export const buttonHeight = {
   height: '3.25rem', // 52px
+}
+
+export const inputHeight = {
+  height: '3rem', // 48px
 }
 
 export const font = {
