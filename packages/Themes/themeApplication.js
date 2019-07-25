8f7a8e80ed2cc67b3bc215ee7ed8a019c50143c3
Enrico Sacchetti <enrico.sacchetti@telus.com>
2019-07-25 12:33:49 -0400

    feat(core-themes): add form styles

--- packages/Themes/themeApplication.js
+++ packages/Themes/themeApplication.js
@@ -3,12 +3,25 @@ import * as colors from '@tds/core-colours'
 export default {
   colors,
   spacing: {
-    padding2: '0 1rem',
+    buttons: {
+      padding: '0rem 1rem',
+    },
+    Box: {
+      '1': '2px',
+      '2': 1,
+    },
   },
   sizing: {
     buttons: {
       height: '40px',
       minWidth: '150px',
+    },
+    forms: {
+      labelTextSize: 'small',
+      Box1: {
+        paddingWithFeedbackIcon: '0.25rem 0.75rem',
+        paddingWithoutFeedbackIcon: '0.25rem 3rem 0.25rem 0.75rem',
+      },
     },
   },
 }
--- packages/Themes/themeMarketing.js
+++ packages/Themes/themeMarketing.js
@@ -3,12 +3,25 @@ import * as colors from '@tds/core-colours'
 export default {
   colors,
   spacing: {
-    padding2: '0 2rem',
+    buttons: {
+      padding: '0 2rem',
+    },
+    Box: {
+      '1': '4px',
+      '2': 2,
+    },
   },
   sizing: {
     buttons: {
       height: '52px',
       minWidth: '180px',
+    },
+    forms: {
+      labelTextSize: 'medium',
+      Box1: {
+        paddingWithFeedbackIcon: '0.875rem 3rem 0.875rem 1rem',
+        paddingWithoutFeedbackIcon: '0.875rem 1rem',
+      },
     },
   },
 }
