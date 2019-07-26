6f3f463958b77ea4ec51a2adde276ededffc4510
Jordan Raffoul <me@jordanraffoul.com>
2019-07-26 14:39:47 -0400

    feat(core-css-reset): upgrade to styled-components
    BREAKING CHANGE: upgrade to styled-components
    BREAKING CHANGE: remove all Sass styles and mixins

--- packages/css-reset/index.js
+++ packages/css-reset/index.js
@@ -1,3 +1,16 @@
 /* eslint-disable import/prefer-default-export */
 
+import { createGlobalStyle } from 'styled-components'
+
+import reset from './reset'
+import fontFaces from './fontFaces'
+import globals from './globals'
+
+import { flexMain } from './globalStyledComponents'
+
+const Reset = createGlobalStyle(reset, fontFaces, globals)
+const GlobalFlexMain = createGlobalStyle(flexMain)
+
 export { default as fonts } from './fonts'
+export { GlobalFlexMain }
+export default Reset
