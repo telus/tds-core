3c586ed287aceef17ec1aa980ac7fa8c455c517f
Marco Donnici <marcod1419@gmail.com>
2019-07-31 15:02:51 -0400

    chore(core-terms-and-conditions): improve render content variable naming

--- packages/TermsAndConditions/renderContent.jsx
+++ packages/TermsAndConditions/renderContent.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 import Link from '@tds/core-link'
 
 const renderContent = c => {
-  const regex = /<\s*a([^>]*)>(.*?)<\s*\/\s*a>/g
+  const tagRegex = /<\s*a([^>]*)>(.*?)<\s*\/\s*a>/g
   const attributeRegex = /(\w+)\s*=\s*((["'])(.*?)\3|(?=\s|\/>))/g
   // split into attributes, and anchor text
-  const parts = c.split(regex)
+  const parts = c.split(tagRegex)
 
   // start with first anchor text
   for (let i = 2; i < parts.length; i += 3) {
@@ -23,7 +23,7 @@ const renderContent = c => {
         {parts[i]}
       </Link>
     )
-    // delete attriute part
+    // delete attribute part
     parts.splice(i - 1, 1)
   }
   return parts
