22fbc1639067926efb87b5b0dfc3b57d68815a68
Enrico Sacchetti <enrico.sacchetti@telus.com>
2019-08-01 14:51:32 -0400

    refactor(core-terms-and-conditions): abstract StyledClickable
    use StyledClickable as a proper SC selector, and reuse styles where possible

--- packages/TermsAndConditions/StyledClickable.jsx
+++ packages/TermsAndConditions/StyledClickable.jsx
@@ -1,13 +1,9 @@
 import styled from 'styled-components'
 
-import { colorWhite } from '@tds/core-colours'
 import { buttons } from '@tds/shared-styles'
 
 const StyledClickable = styled.button(buttons.noStyle, {
-  width: '100%',
   display: 'flex',
-  justifyContent: 'center',
-  backgroundColor: colorWhite,
 })
 
 export default StyledClickable
