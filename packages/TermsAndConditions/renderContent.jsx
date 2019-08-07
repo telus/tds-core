b3aa0b0935957dc86e7ec7ceb1e1c433f7df3336
Jordan Raffoul <me@jordanraffoul.com>
2019-08-07 15:09:36 -0400

    fix(core-terms-and-conditions): renderContent handles multiple anchors

--- packages/TermsAndConditions/renderContent.jsx
+++ packages/TermsAndConditions/renderContent.jsx
@@ -6,25 +6,31 @@ const renderContent = c => {
   const attributeRegex = /(\w+)\s*=\s*((["'])(.*?)\3|(?=\s|\/>))/g
   // split into attributes, and anchor text
   const parts = c.split(tagRegex)
-
-  // start with first anchor text
+  if (parts.length === 1) {
+    return parts
+  }
+  // start with first anchor text, attributes will be in the previous part
   for (let i = 2; i < parts.length; i += 3) {
     const o = {}
     // get attributes from previous part
     const attributes = parts[i - 1].trim()
 
     // create object from attributes
-    attributes.match(attributeRegex).forEach(attribute => {
-      const split = attribute.split('=')
-      o[split[0]] = split[1].substr(1, split[1].length - 2)
-    })
+    const matchedAttributes = attributes.match(attributeRegex)
+    if (matchedAttributes) {
+      matchedAttributes.forEach(attribute => {
+        const split = attribute.split('=')
+        o[split[0]] = split[1].substr(1, split[1].length - 2)
+      })
+    }
+    // remove anchor attributes from parts
+    parts[i - 1] = undefined
+    // replace anchor text with Link in parts
     parts[i] = (
       <Link {...o} key={i}>
         {parts[i]}
       </Link>
     )
-    // delete attribute part
-    parts.splice(i - 1, 1)
   }
   return parts
 }
