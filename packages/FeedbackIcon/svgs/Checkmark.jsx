fc8035fc0ad3ee8ced744e0aa6591f0b486eb04d
Anatolii Gorovyi <anatolii.gorovyi@telus.com>
2019-08-13 11:20:02 -0400

    chore(core-feedback-icon): replace svgs with clean exports

--- packages/FeedbackIcon/svgs/Checkmark.jsx
+++ packages/FeedbackIcon/svgs/Checkmark.jsx
@@ -1,43 +1,18 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-import { colorWhite, colorShark } from '@tds/core-colours'
-
 import FeedbackIcon from '../FeedbackIcon'
 import FeedbackIconSVG from '../FeedbackIconSVG'
-import uniqueId from '../uniqueId'
 
 const Checkmark = ({ a11yText, ...rest }) => {
-  const checkA = uniqueId('check-a')
-  const checkB = uniqueId('check-b')
   return (
     <FeedbackIcon {...rest}>
-      <FeedbackIconSVG
-        a11yText={a11yText}
-        xmlns="http://www.w3.org/2000/svg"
-        xmlnsXlink="http://www.w3.org/1999/xlink"
-        width="16"
-        height="16"
-        viewBox="0 0 16 16"
-      >
-        <defs>
-          <path
-            id={checkA}
-            d="M4.80704257,11.0724193 C4.78158917,11.1102831 4.75124181,11.1458298 4.71601955,11.1781862 L4.69030272,11.2018105 L4.58923996,11.3138257 C4.47110567,11.4447625 4.3131571,11.5069395 4.15716228,11.4993862 C3.99109613,11.5030099 3.82400439,11.4340415 3.70395505,11.295338 L0.158769778,7.19927455 C-0.0576056521,6.94927704 -0.0527868641,6.55339761 0.173024834,6.3113094 C0.397273114,6.07089731 0.763972727,6.0742436 0.984840742,6.31103179 L4.13955796,9.69314104 L13.0124245,0.180718232 C13.2371196,-0.0601728497 13.6006798,-0.0609687568 13.827974,0.182708796 C14.0536945,0.424699236 14.0580195,0.818890512 13.8366025,1.06430301 L4.80704257,11.0724193 Z"
-          />
-        </defs>
-        <g fill="none" fillRule="evenodd">
-          <g transform="translate(2 2)">
-            <mask id={checkB} fill={colorWhite}>
-              <use xlinkHref={`#${checkA}`} />
-            </mask>
-            <use fill={colorShark} xlinkHref={`#${checkA}`} />
-            <g fill="#2B8000" mask={`url(#${checkB})`}>
-              <rect width="14" height="12" />
-            </g>
-          </g>
-          <rect width="16" height="16" />
-        </g>
+      <FeedbackIconSVG a11yText={a11yText} width="16" height="16" viewBox="0 0 16 16">
+        <path
+          fill="#2B8000"
+          fillRule="evenodd"
+          d="M5.807 13.072a.592.592 0 0 1-.091.106l-.026.024-.1.112a.545.545 0 0 1-.433.185.581.581 0 0 1-.453-.204L1.159 9.2a.677.677 0 0 1 .014-.888.55.55 0 0 1 .812 0l3.155 3.382 8.872-9.512a.548.548 0 0 1 .816.002c.226.242.23.636.009.881l-9.03 10.008z"
+        />
       </FeedbackIconSVG>
     </FeedbackIcon>
   )
--- packages/FeedbackIcon/svgs/LocationIneligible.jsx
+++ packages/FeedbackIcon/svgs/LocationIneligible.jsx
@@ -16,27 +16,27 @@ const copyDictionary = {
   },
 }
 
-const LocationSuccess = ({ copy, ...rest }) => (
-  <FeedbackIcon {...rest}>
-    <FeedbackIconSVG
-      a11yText={getCopy(copyDictionary, copy).a11yText}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-    >
-      <path
-        fill={colorCardinal}
-        fillRule="evenodd"
-        transform="translate(3)"
-        d="M8.9995,9.2075 L7.353,10.854 C7.256,10.951 7.128,11 7,11 C6.872,11 6.744,10.951 6.646,10.854 C6.451,10.659 6.451,10.342 6.646,10.147 L8.2925,8.5005 L6.646,6.854 C6.451,6.659 6.451,6.342 6.646,6.147 C6.841,5.952 7.158,5.952 7.353,6.147 L8.9995,7.7935 L10.646,6.147 C10.841,5.952 11.158,5.952 11.353,6.147 C11.548,6.342 11.548,6.659 11.353,6.854 L9.7065,8.5005 L11.353,10.147 C11.548,10.342 11.548,10.659 11.353,10.854 C11.256,10.951 11.128,11 11,11 C10.872,11 10.744,10.951 10.646,10.854 L8.9995,9.2075 Z M9,24 C8.884,24 8.767,23.959 8.673,23.878 C8.339,23.59 0.5,16.718 0.5,8.5 C0.5,3.813 4.313,0 9,0 C13.687,0 17.5,3.813 17.5,8.5 C17.5,16.718 9.661,23.59 9.327,23.878 C9.233,23.959 9.116,24 9,24 Z M9,1 C4.864,1 1.5,4.364 1.5,8.5 C1.5,15.342 7.516,21.426 9,22.822 C10.483,21.425 16.5,15.334 16.5,8.5 C16.5,4.364 13.136,1 9,1 Z M9,14 C5.968,14 3.5,11.533 3.5,8.5 C3.5,5.467 5.968,3 9,3 C12.032,3 14.5,5.467 14.5,8.5 C14.5,11.533 12.032,14 9,14 Z M9,4 C6.519,4 4.5,6.019 4.5,8.5 C4.5,10.981 6.519,13 9,13 C11.481,13 13.5,10.981 13.5,8.5 C13.5,6.019 11.481,4 9,4 Z"
-      />
-    </FeedbackIconSVG>
-  </FeedbackIcon>
-)
-LocationSuccess.displayName = 'LocationSuccess'
+const LocationIneligible = ({ copy, ...rest }) => {
+  return (
+    <FeedbackIcon {...rest}>
+      <FeedbackIconSVG
+        a11yText={getCopy(copyDictionary, copy).a11yText}
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+      >
+        <path
+          fill={colorCardinal}
+          fillRule="evenodd"
+          d="M12.5 9.207l-1.647 1.647a.498.498 0 0 1-.707 0 .5.5 0 0 1 0-.707l1.646-1.646-1.646-1.647a.5.5 0 0 1 .707-.707l1.646 1.646 1.647-1.646a.5.5 0 0 1 .707.707l-1.646 1.647 1.646 1.646a.5.5 0 0 1-.707.707l-1.647-1.646zM12.5 24a.501.501 0 0 1-.327-.122C11.839 23.59 4 16.718 4 8.5 4 3.813 7.813 0 12.5 0 17.187 0 21 3.813 21 8.5c0 8.218-7.839 15.09-8.173 15.378A.501.501 0 0 1 12.5 24zm0-23C8.364 1 5 4.364 5 8.5c0 6.842 6.016 12.926 7.5 14.322C13.983 21.425 20 15.334 20 8.5 20 4.364 16.636 1 12.5 1zm0 13A5.506 5.506 0 0 1 7 8.5C7 5.467 9.468 3 12.5 3S18 5.467 18 8.5 15.532 14 12.5 14zm0-10A4.505 4.505 0 0 0 8 8.5c0 2.481 2.019 4.5 4.5 4.5S17 10.981 17 8.5 14.981 4 12.5 4z"
+        />
+      </FeedbackIconSVG>
+    </FeedbackIcon>
+  )
+}
+LocationIneligible.displayName = 'LocationIneligible'
 
-LocationSuccess.propTypes = {
+LocationIneligible.propTypes = {
   copy: PropTypes.oneOfType([
     PropTypes.oneOf(['en', 'fr']),
     PropTypes.shape({
@@ -45,4 +45,4 @@ LocationSuccess.propTypes = {
   ]).isRequired,
 }
 
-export default LocationSuccess
+export default LocationIneligible
