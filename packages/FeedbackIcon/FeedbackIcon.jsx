f9cfd92168894baa8c3c5f3d807fb60254ada135
Jordan Raffoul <me@jordanraffoul.com>
2019-08-30 15:32:21 -0400

    refactor(core-feedback-icon): use safeRest from @tds/util-helpers

--- packages/FeedbackIcon/FeedbackIcon.jsx
+++ packages/FeedbackIcon/FeedbackIcon.jsx
@@ -1,8 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { getCopy } from '@tds/util-helpers'
+import { getCopy, safeRest } from '@tds/util-helpers'
 
-import safeRest from '../../shared/utils/safeRest'
 import { warn } from '../../shared/utils/warn'
 
 /**
