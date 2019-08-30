3f0b3acc731f6222805e74f0ec045cc8069ecc34
Jordan Raffoul <me@jordanraffoul.com>
2019-08-30 14:34:37 -0400

    feat(util-helpers): add safeRest to util-helpers

--- packages/helpers/index.js
+++ packages/helpers/index.js
@@ -1,3 +1,4 @@
 /* eslint-disable import/prefer-default-export */
 export { default as getCopy } from './getCopy'
 export { default as uniqueId } from './uniqueId'
+export { default as safeRest } from './safeRest'
