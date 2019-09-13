034e898a81388692589f5a4f395ff5143452e811
Anatolii Gorovyi <anatolii.gorovyi@telus.com>
2019-09-13 15:48:57 -0400

    feat(shared-hocs): add withForwardedRef hoc

--- packages/hocs/index.js
+++ packages/hocs/index.js
@@ -1,2 +1,3 @@
 export { default as withFocusTrap } from './withFocusTrap'
 export { default as withStyledComponent } from './withStyledComponent'
+export { default as withForwardedRef } from './withForwardedRef'
