f814a641b9077cc6f18d927cb1098790f0678edd
Jordan Raffoul <me@jordanraffoul.com>
2019-09-26 15:59:21 -0400

    refactor(core-feedback-icon): update colours to use new greyscale/rajah

--- packages/FeedbackIcon/svgs/NotificationWarning.jsx
+++ packages/FeedbackIcon/svgs/NotificationWarning.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import { colorDarkRaja } from '@tds/core-colours'
+import { colorRajahDark } from '@tds/core-colours'
 
 import FeedbackIcon from '../FeedbackIcon'
 
@@ -22,7 +22,7 @@ const NotificationWarning = props => (
     viewBox="0 0 20 20"
   >
     <path
-      fill={colorDarkRaja}
+      fill={colorRajahDark}
       fillRule="evenodd"
       d="M10.878 1.61l8.315 15.244a1 1 0 0 1-.878 1.48H1.685a1 1 0 0 1-.878-1.48L9.122 1.61a1 1 0 0 1 1.756 0zM10 16.794c.46 0 .833-.402.833-.898 0-.495-.373-.897-.833-.897-.46 0-.833.402-.833.897 0 .496.373.898.833.898zm-.022-2.885c.347 0 .63-.297.64-.67l.179-6.698c.01-.388-.28-.709-.64-.709h-.35c-.361 0-.65.32-.64.708l.171 6.699c.01.373.294.67.64.67z"
     />
