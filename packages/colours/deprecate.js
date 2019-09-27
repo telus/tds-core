44a1ccf1a7951b1f725658015683cbdb58fdf18a
Jordan Raffoul <me@jordanraffoul.com>
2019-09-27 09:26:32 -0400

    chore(core-colours): add postinstall deprecation notice

---
+++ packages/colours/deprecate.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-console */
+console.warn(`[TDS] @tds/core-colours: The following colours have been deprecated, please see https://tds.telus.com/components/index.html#!/Colours for their alternatives:
+  colorShark,
+  colorShuttleGrey,
+  colorAthens,
+  colorGainsboro,
+  colorDarkRaja,
+  colorRaja
+  colorLightRaja
+`)
