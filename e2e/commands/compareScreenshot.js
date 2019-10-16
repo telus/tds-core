978f1c7368b37d8110c4912bb82fd03c60772ad1
Jordan Raffoul <me@jordanraffoul.com>
2019-10-16 15:39:26 -0400

    chore(e2e): remove resize window from compareScreenshot command

--- e2e/commands/compareScreenshot.js
+++ e2e/commands/compareScreenshot.js
@@ -19,19 +19,6 @@ const ensureFolderExists = (path, folderName) => {
   }
 }
 
-const resizeWindow = browser => {
-  return browser.element('css selector', 'html', element => {
-    browser.elementIdSize(element.value.ELEMENT, pageSize => {
-      const width = pageSize.value.width
-      const height = pageSize.value.height
-
-      // add a pause to ensure any process / event is finished processing
-      browser.resizeWindow(width, height)
-      browser.pause(500)
-    })
-  })
-}
-
 const takeScreenshot = (resultPath, browser, selector, callback) => {
   return browser.element('css selector', selector, element => {
     browser.saveScreenshot(resultPath, () => {
@@ -40,16 +27,14 @@ const takeScreenshot = (resultPath, browser, selector, callback) => {
   })
 }
 
-exports.command = function compareScreenshot(componentName, browserName, browserVersion, isMobile) {
-  const fileName = `${browserName}_${browserVersion}${isMobile ? '-mobile' : ''}.png`
+exports.command = function compareScreenshot(componentName, browserName, browserVersion) {
+  const fileName = `${browserName}_${browserVersion}.png`
   const folders = getVisualRegressionFolders(componentName)
   const resultScreenshotPath = resolve(folders.results, fileName)
 
   ensureFolderExists(folders.baseline, 'baseline')
   ensureFolderExists(folders.results, 'result')
   ensureFolderExists(folders.diffs, 'diff')
-
-  resizeWindow(this)
 
   takeScreenshot(resultScreenshotPath, this, rootSelector, () => {
     this.verify.compareScreenshot(componentName, fileName)
