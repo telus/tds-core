57624dc57c52b116d7295f1665997325af54ce54
Jordan Raffoul <me@jordanraffoul.com>
2019-10-24 15:32:01 -0400

    fix(shared-animation): hide content from screenreaders when exited

--- packages/Animation/FadeAndReveal.jsx
+++ packages/Animation/FadeAndReveal.jsx
@@ -3,16 +3,17 @@ import PropTypes from 'prop-types'
 import { Transition } from 'react-transition-group'
 import styled from 'styled-components'
 
-const StyledContainer = styled.div({
+export const StyledContainer = styled.div({
   '@media (prefers-reduced-motion: reduce)': {
     transition: 'none !important',
   },
 })
-const defaultStyle = (duration, timeout) => ({
+const defaultStyle = (duration, timeout, state) => ({
   transition: `height ${duration || timeout}ms, opacity ${duration || timeout}ms ease-in-out`,
   opacity: 0,
   height: 0,
   overflow: 'hidden',
+  visibility: state === 'exited' || state === 'exiting' ? 'hidden' : 'visible',
 })
 
 const transitionStyles = height => (duration, timeout) => ({
@@ -31,7 +32,7 @@ const FadeAndReveal = ({ delay, height, children, ...rest }) => (
     {state => (
       <StyledContainer
         style={{
-          ...defaultStyle(rest.duration, rest.timeout),
+          ...defaultStyle(rest.duration, rest.timeout, state),
           ...transitionStyles(height)(rest.duration, rest.timeout)[state],
         }}
         aria-hidden={state === 'exiting' || state === 'exited'}
---
+++ packages/Animation/__tests__/FadeAndReveal.spec.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import { render, mount } from 'enzyme'
+
+import FadeAndReveal, { StyledContainer } from '../FadeAndReveal'
+
+describe('FadeAndReveal', () => {
+  it('renders when the transition is triggered', () => {
+    const fadeAndReveal = render(
+      <FadeAndReveal timeout={500} duration={100} height={500} in={true}>
+        {() => <div>Content to fade</div>}
+      </FadeAndReveal>
+    )
+
+    expect(fadeAndReveal).toMatchSnapshot()
+  })
+
+  it('renders when the transition is not triggered', () => {
+    const fadeAndReveal = render(
+      <FadeAndReveal timeout={500} duration={100} height={500} in={false}>
+        {() => <div>Content to fade</div>}
+      </FadeAndReveal>
+    )
+
+    expect(fadeAndReveal).toMatchSnapshot()
+  })
+
+  it('should hide content from screenreaders when exited', () => {
+    const fadeAndReveal = mount(
+      <FadeAndReveal timeout={500} duration={100} height={500} in={false}>
+        {() => <div>Content to fade</div>}
+      </FadeAndReveal>
+    )
+
+    const styledContainer = fadeAndReveal.find(StyledContainer)
+    expect(styledContainer).toHaveProp('aria-hidden', true)
+    expect(styledContainer.prop('style')).toHaveProperty('visibility', 'hidden')
+  })
+
+  it('should display content from screenreaders when entered', () => {
+    const fadeAndReveal = mount(
+      <FadeAndReveal timeout={500} duration={100} height={500} in={true}>
+        {() => <div>Content to fade</div>}
+      </FadeAndReveal>
+    )
+
+    const styledContainer = fadeAndReveal.find(StyledContainer)
+    expect(styledContainer).toHaveProp('aria-hidden', false)
+    expect(styledContainer.prop('style')).toHaveProperty('visibility', 'visible')
+  })
+})
