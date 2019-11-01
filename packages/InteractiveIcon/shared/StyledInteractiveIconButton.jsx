fe969854e8c8b6e08f7616f3e2cc02956cff557a
Enrico Sacchetti <enrico.sacchetti@telus.com>
2019-11-01 17:15:23 -0400

    chore(core-interactive-icon): fix tooltip marginLeft calculation
    Also provide accessible focus state for inverted icons, as well as in Firefox

--- packages/InteractiveIcon/shared/StyledInteractiveIconButton.jsx
+++ packages/InteractiveIcon/shared/StyledInteractiveIconButton.jsx
@@ -1,8 +1,24 @@
 import styled from 'styled-components'
 
+import { colorWhite } from '@tds/core-colours'
 import { buttons } from '@tds/shared-styles'
 
-const getOutline = ({ variant }) => variant !== 'inverted' && { outline: 'none' }
+const getOutline = ({ variant }) => {
+  if (variant !== 'inverted') {
+    return {
+      outline: 'none',
+      '&:focus::-moz-focus-inner': {
+        border: 0,
+      },
+    }
+  }
+
+  return {
+    '&:focus': {
+      outline: `0.125rem solid ${colorWhite}`,
+    },
+  }
+}
 
 const StyledInteractiveIconButton = styled.button(buttons.noStyle, getOutline, {
   width: '2.5rem',
