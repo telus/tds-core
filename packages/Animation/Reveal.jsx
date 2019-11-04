a56e24ff153bdeaf98e12e68d9f12b728ab9e9ce
Jordan Raffoul <me@jordanraffoul.com>
2019-11-04 12:05:36 -0500

    feat(shared-animation): cleanup Reveal, add delay feature

--- packages/Animation/Reveal.jsx
+++ packages/Animation/Reveal.jsx
@@ -2,54 +2,50 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { Transition } from 'react-transition-group'
 
-const defaultStyle = (duration, timeout) => ({
-  transition: `height ${duration || timeout}ms`,
-  height: 0,
-  overflow: 'hidden',
+const defaultStyle = (timeout, delay) => {
+  return {
+    transition: `height ${timeout}ms ${delay ? `${delay}ms` : ''}`,
+    overflow: 'hidden',
+  }
+}
+
+const transitionStyles = height => ({
+  entering: { height: `${height}px` },
+  entered: { height: `${height}px`, overflow: 'visible' },
+  exiting: { height: 0 },
+  exited: { height: 0, visibility: 'hidden' },
 })
 
-const hiddenContent = {
-  visibility: 'hidden',
+const Reveal = ({ duration, timeout, height, delay, children, ...rest }) => {
+  const transitionTimeout = duration || timeout
+  return (
+    <Transition {...rest} timeout={transitionTimeout + delay}>
+      {status => (
+        <div
+          style={{
+            ...defaultStyle(transitionTimeout, delay),
+            ...transitionStyles(height)[status],
+          }}
+          aria-hidden={status === 'exited'}
+          data-testid="childrenContainer"
+        >
+          {children()}
+        </div>
+      )}
+    </Transition>
+  )
 }
-
-const transitionStyles = height => {
-  const styles = {
-    height: `${height}px`,
-  }
-
-  return {
-    entering: styles,
-    entered: { height: 'auto', overflow: 'visible' },
-    exiting: styles,
-    exited: hiddenContent,
-  }
-}
-
-const Reveal = ({ height, children, ...rest }) => (
-  <Transition {...rest}>
-    {status => (
-      <div
-        style={{
-          ...defaultStyle(rest.duration, rest.timeout, rest.in),
-          ...transitionStyles(height)[status],
-        }}
-        aria-hidden={status === 'exited'}
-        data-testid="childrenContainer"
-      >
-        {children()}
-      </div>
-    )}
-  </Transition>
-)
 Reveal.propTypes = {
   timeout: PropTypes.number.isRequired,
+  delay: PropTypes.number,
   duration: PropTypes.number,
-  height: PropTypes.number.isRequired,
+  height: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
   children: PropTypes.func.isRequired,
 }
 
 Reveal.defaultProps = {
   duration: undefined,
+  delay: undefined,
 }
 
 export default Reveal
