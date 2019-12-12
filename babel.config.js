9011ff1d7c27c548b10136004ae14e188d376eaf
Marco Donnici <marcod1419@gmail.com>
2019-12-12 13:19:28 -0500

    chore(other): add back transform runtime after workaround

--- babel.config.js
+++ babel.config.js
@@ -4,6 +4,7 @@ module.exports = function(api) {
   return {
     presets: ['@babel/preset-env', '@babel/preset-react'],
     plugins: [
+      '@babel/plugin-transform-runtime',
       ['@babel/plugin-proposal-object-rest-spread', { loose: true }],
       '@babel/plugin-proposal-class-properties',
       'babel-plugin-styled-components',
