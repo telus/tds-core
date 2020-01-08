b95196aab5ff9ee65eaa99e9624362edd965e5ff
Marco Donnici <marcod1419@gmail.com>
2020-01-08 14:00:19 -0500

    test(e2e): update interactive e2e cartesian for dependent icon

--- e2e/visual/components/CartesianInteractiveIcon.jsx
+++ e2e/visual/components/CartesianInteractiveIcon.jsx
@@ -1,4 +1,4 @@
-/* eslint-disable react/jsx-key */
+/* eslint-disable react/jsx-key, react/no-children-prop */
 import React from 'react'
 import styled from 'styled-components'
 
@@ -8,6 +8,9 @@ import StyledContainer from '../shared/StyledContainer'
 
 import * as icons from '../../../packages/InteractiveIcon'
 
+import Link from '../../../packages/Link'
+import * as dependentIcons from '../../../packages/InteractiveIcon/svgs/Dependent'
+
 const StyledIconContainer = styled(StyledContainer)({
   padding: 12,
   width: 'auto',
@@ -15,20 +18,37 @@ const StyledIconContainer = styled(StyledContainer)({
 
 const CartesianInteractiveIcon = props => (
   <React.Fragment>
-    {Object.keys(icons)
-      .filter(icon => icon !== 'default')
-      .map(icon => (
-        <Cartesian
-          {...props}
-          container={StyledIconContainer}
-          component={icons[icon]}
-          a11yText="A11yText"
-          variant={['default', 'alternative', 'inverted']}
-          tag={['button', 'a']}
-          copy="en"
-          numItems={2}
-        />
-      ))}
+    <React.Fragment>
+      {Object.keys(icons)
+        .filter(icon => icon !== 'default')
+        .map(icon => (
+          <Cartesian
+            {...props}
+            container={StyledIconContainer}
+            component={icons[icon]}
+            a11yText="A11yText"
+            variant={['default', 'alternative', 'inverted']}
+            tag={['button', 'a']}
+            copy="en"
+            numItems={2}
+          />
+        ))}
+    </React.Fragment>
+    <React.Fragment>
+      {Object.keys(dependentIcons)
+        .filter(dependentIcon => dependentIcon !== 'default')
+        .map(dependentIcon => (
+          <Cartesian
+            {...props}
+            component={Link}
+            href="http://tds.telus.com"
+            invert={[true, false]}
+            children="TELUS Design System"
+            icon={dependentIcons[dependentIcon]}
+            iconPosition={['left', 'right']}
+          />
+        ))}
+    </React.Fragment>
   </React.Fragment>
 )
 
