c9b53dd7542c7482eb9a1d68dde254c5342fa5e4
Marco Donnici <marcod1419@gmail.com>
2020-01-10 15:43:33 -0500

    test(e2e): add flex to cartesian paragraph

--- e2e/visual/components/CartesianParagraph.jsx
+++ e2e/visual/components/CartesianParagraph.jsx
@@ -5,16 +5,23 @@ import { Cartesian } from '@compositor/kit'
 
 import Paragraph from '../../../packages/Paragraph'
 
-const CartesianUnorderedList = props => (
-  <Cartesian
-    {...props}
-    component={Paragraph}
-    bold={[false, true]}
-    size={['small', 'medium', 'large']}
-    align={['left', 'center', 'right']}
-    invert={[false, true]}
-    children="Our commitment to service is demonstrated in everything we do."
-  />
+const CartesianParagraph = props => (
+  <div style={{ width: '50%' }}>
+    <Cartesian {...props} component={Paragraph} />
+  </div>
 )
 
-export default { name: 'Paragraph', Component: CartesianUnorderedList }
+const ParagraphContainer = () => (
+  <div style={{ display: 'flex', flexWrap: 'wrap', height: '100%' }}>
+    <Cartesian
+      component={CartesianParagraph}
+      bold={[false, true]}
+      size={['small', 'medium', 'large']}
+      align={['left', 'center', 'right']}
+      invert={[false, true]}
+      children="Our commitment to service is demonstrated in everything we do."
+    />
+  </div>
+)
+
+export default { name: 'Paragraph', Component: ParagraphContainer }
