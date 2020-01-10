0e93f9607178338b59cdd91ae17d83a713dba732
Marco Donnici <marcod1419@gmail.com>
2020-01-10 15:11:03 -0500

    test(e2e): add flex to image cartesian

--- e2e/visual/components/CartesianImage.jsx
+++ e2e/visual/components/CartesianImage.jsx
@@ -6,15 +6,22 @@ import { Cartesian } from '@compositor/kit'
 import Image from '../../../packages/Image'
 
 const CartesianImage = props => (
-  <Cartesian
-    {...props}
-    component={Image}
-    src="https://tds.telus.com/components/image-example.jpg"
-    alt="Image of co-workers collaborating"
-    width={[300, 600]}
-    height={[300, 600]}
-    rounded={[undefined, 'circle', 'corners']}
-  />
+  <div style={{ width: '25%' }}>
+    <Cartesian {...props} component={Image} />
+  </div>
 )
 
-export default { name: 'Image', Component: CartesianImage }
+const ImageContainer = () => (
+  <div style={{ display: 'flex', flexWrap: 'wrap', height: '100%' }}>
+    <Cartesian
+      component={CartesianImage}
+      src="https://tds.telus.com/components/image-example.jpg"
+      alt="Image of co-workers collaborating"
+      width={[300, 600]}
+      height={[300, 600]}
+      rounded={[undefined, 'circle', 'corners']}
+    />
+  </div>
+)
+
+export default { name: 'Image', Component: ImageContainer }
