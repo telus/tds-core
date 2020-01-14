cceea96ad9ad33b1482e75633ed34dd238af4e86
Marco Donnici <marcod1419@gmail.com>
2020-01-14 16:36:25 -0500

    test(e2e): update cartesian text component to wrap

--- e2e/visual/components/CartesianText.jsx
+++ e2e/visual/components/CartesianText.jsx
@@ -6,15 +6,21 @@ import { Cartesian } from '@compositor/kit'
 import Text from '../../../packages/Text'
 
 const CartesianText = props => (
-  <Cartesian
-    {...props}
-    component={Text}
-    block={[false, true]}
-    bold={[false, true]}
-    size={['base', 'small', 'medium', 'large']}
-    invert={[false, true]}
-    children="Taxes and pay-per-use charges are extra."
-  />
+  <div style={{ width: '25%' }}>
+    <Cartesian {...props} component={Text} />
+  </div>
+)
+const CartesianContainer = () => (
+  <div style={{ display: 'flex', flexWrap: 'wrap', height: '100%' }}>
+    <Cartesian
+      component={CartesianText}
+      block={[false, true]}
+      bold={[false, true]}
+      size={['base', 'small', 'medium', 'large']}
+      invert={[false, true]}
+      children="Taxes and pay-per-use charges are extra."
+    />
+  </div>
 )
 
-export default { name: 'Text', Component: CartesianText }
+export default { name: 'Text', Component: CartesianContainer }
