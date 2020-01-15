11dabb9083d7e0dcaff9a8cd7606a1857b22f5a9
Marco Donnici <marcod1419@gmail.com>
2020-01-15 14:32:20 -0500

    test(e2e): update card cartesian component

--- e2e/visual/components/CartesianCard.jsx
+++ e2e/visual/components/CartesianCard.jsx
@@ -6,12 +6,28 @@ import { Cartesian } from '@compositor/kit'
 import Card from '../../../packages/Card'
 
 const CartesianCard = props => (
-  <Cartesian
-    {...props}
-    component={Card}
-    variant={['default', 'defaultWithBorder', 'branded', 'alternative']}
-    children="Get a new smartphone for $0 on a 2-year plan"
-  />
+  <div style={{ width: '20%', maxHeight: 250 }}>
+    <Cartesian {...props} component={Card} />
+  </div>
 )
 
-export default { name: 'Card', Component: CartesianCard }
+const CartesianContainer = () => (
+  <div style={{ display: 'flex', flexWrap: 'wrap', height: '100%' }}>
+    <Cartesian
+      component={CartesianCard}
+      variant={[
+        'white',
+        'lavender',
+        'grey',
+        'default',
+        'branded',
+        'alternative',
+        'defaultWithBorder',
+      ]}
+      fullHeight={[true, false]}
+      spacing={[['default', 'narrow', 'compact', 'intermediate']]}
+      children="Get a new smartphone for $0 on a 2-year plan"
+    />
+  </div>
+)
+export default { name: 'Card', Component: CartesianContainer }
