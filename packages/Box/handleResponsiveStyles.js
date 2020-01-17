a35a07b386b19cae718e95971b66e7510954d461
Marco Donnici <marcod1419@gmail.com>
2020-01-17 15:12:37 -0500

    chore(core-box): simplify if statement in responsive handler

--- packages/Box/handleResponsiveStyles.js
+++ packages/Box/handleResponsiveStyles.js
@@ -6,15 +6,13 @@ const DESKTOP_BREAKPOINTS = ['md', 'lg', 'xl']
 export const isMobileBreakpoint = breakpoint => MOBILE_BREAKPOINTS.indexOf(breakpoint) !== -1
 export const isDesktopBreakpoint = breakpoint => DESKTOP_BREAKPOINTS.indexOf(breakpoint) !== -1
 
-const isNumber = x => typeof x === 'number'
-
 const handleBoundaryCrossing = (acc, curr) => {
   if (
     isMobileBreakpoint(curr.from) &&
     ((curr.until !== 'md' && isDesktopBreakpoint(curr.until)) || typeof curr.until === 'undefined')
   ) {
     const props = Object.keys(curr.props).filter(
-      prop => isNumber(curr.props[prop]) && curr.props[prop] > 3
+      prop => typeof curr.props[prop] === 'number' && curr.props[prop] > 3
     )
     if (props.length !== 0) {
       const mobileBreakpoint = Object.assign({}, curr, { props: curr.props })
