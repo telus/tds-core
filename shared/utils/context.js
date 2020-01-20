1368a311770639f3b76945d2bd6d37c6523d2d9e
Jordan Raffoul <me@jordanraffoul.com>
2020-01-20 15:23:18 -0500

    chore(shared): remove context.js

--- shared/utils/context.js
+++
@@ -1,6 +0,0 @@
-import { createContext } from 'react'
-
-// This module helps establish a predefined provider and consumer when used in an application
-// Be cautious about using this in multiple components as it may share only a single context among every consumer
-const { Provider, Consumer } = createContext({})
-export { Provider, Consumer }
