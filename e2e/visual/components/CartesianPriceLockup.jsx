26474fb18a3e91fff57c711a9c10dd04e2bd03a4
Jordan Raffoul <me@jordanraffoul.com>
2020-03-23 14:52:49 -0400

    test(e2e): update PriceLockup cartesian

--- e2e/visual/components/CartesianPriceLockup.jsx
+++ e2e/visual/components/CartesianPriceLockup.jsx
@@ -6,24 +6,70 @@ import { Cartesian } from '@compositor/kit'
 import PriceLockup from '../../../packages/PriceLockup'
 import FootnoteLink from '../../../packages/TermsAndConditions/FootnoteLink/FootnoteLink'
 
-const CartesianPriceLockup = props => (
-  <div style={{ width: '10%' }}>
+const renderCartesianPriceLockup = options => props => (
+  <div style={{ minWidth: options.minWidth, padding: '0 8px 8px 0' }}>
     <PriceLockup {...props} />
   </div>
 )
 
 const PriceLockupContainer = () => (
-  <div style={{ display: 'flex', flexWrap: 'wrap', height: '100%' }}>
-    <Cartesian
-      component={CartesianPriceLockup}
-      size={['small', 'medium', 'large']}
-      signDirection={['left', 'right']}
-      topText={[undefined, 'Starting from']}
-      bottomText={[undefined, 'on a 2-year plan']}
-      rateText={[undefined, 'per month']}
-      price="29"
-      footnoteLinks={[undefined, <FootnoteLink number={[1, 2, 3]} copy="en" onClick={() => {}} />]}
-    />
+  <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+    <div style={{ width: '20%' }}>
+      <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+        <Cartesian
+          component={renderCartesianPriceLockup({ minWidth: '130px' })}
+          size="small"
+          signDirection={['left', 'right']}
+          topText={[undefined, 'Starting from']}
+          bottomText={[undefined, 'on a 2-year plan']}
+          rateText={[undefined, 'per month']}
+          price="29"
+          footnoteLinks={[
+            undefined,
+            <FootnoteLink number={[1, 2]} copy="en" onClick={() => {}} />,
+            <FootnoteLink number={[1, 2, 3, 4]} copy="en" onClick={() => {}} />,
+          ]}
+        />
+      </div>
+    </div>
+
+    <div style={{ width: '25%' }}>
+      <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+        <Cartesian
+          component={renderCartesianPriceLockup({ minWidth: '190px' })}
+          size="medium"
+          signDirection={['left', 'right']}
+          topText={[undefined, 'Starting from']}
+          bottomText={[undefined, 'on a 2-year plan']}
+          rateText={[undefined, 'per month']}
+          price="29"
+          footnoteLinks={[
+            undefined,
+            <FootnoteLink number={[1, 2]} copy="en" onClick={() => {}} />,
+            <FootnoteLink number={[1, 2, 3, 4]} copy="en" onClick={() => {}} />,
+          ]}
+        />
+      </div>
+    </div>
+
+    <div style={{ width: '55%' }}>
+      <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+        <Cartesian
+          component={renderCartesianPriceLockup({ minWidth: '300px' })}
+          size="large"
+          signDirection={['left', 'right']}
+          topText={[undefined, 'Starting from']}
+          bottomText={[undefined, 'on a 2-year plan']}
+          rateText={[undefined, 'per month']}
+          price="29"
+          footnoteLinks={[
+            undefined,
+            <FootnoteLink number={[1, 2]} copy="en" onClick={() => {}} />,
+            <FootnoteLink number={[1, 2, 3, 4]} copy="en" onClick={() => {}} />,
+          ]}
+        />
+      </div>
+    </div>
   </div>
 )
 
