71497e829d1c046f3d7c6ae83d9040c53d3dafd9
Jordan Raffoul <me@jordanraffoul.com>
2020-03-24 16:51:38 -0400

    chore(scripts): add link to e2e tests to tdsbot

--- scripts/circleci/github-pr.js
+++ scripts/circleci/github-pr.js
@@ -3,8 +3,9 @@
 
 // Environment: These variables must be available in the environment.
 // GITHUB_TOKEN
-// TRAVIS_REPO_SLUG
+// CIRCLE_PROJECT_REPONAME
 // CIRCLE_PULL_REQUEST
+// CIRCLE_BRANCH
 
 const request = require('request')
 const { spawnSync } = require('child_process')
@@ -12,10 +13,11 @@ const { spawnSync } = require('child_process')
 if (
   !process.env.GITHUB_TOKEN ||
   !process.env.CIRCLE_PROJECT_REPONAME ||
-  !process.env.CIRCLE_PULL_REQUEST
+  !process.env.CIRCLE_PULL_REQUEST ||
+  !process.env.CIRCLE_BRANCH
 ) {
   console.error(
-    "'GITHUB_TOKEN', 'CIRCLE_PROJECT_REPONAME'., and 'CIRCLE_PULL_REQUEST' must be available in the environment."
+    "'GITHUB_TOKEN', 'CIRCLE_PROJECT_REPONAME', 'CIRCLE_PULL_REQUEST', and 'CIRCLE_BRANCH' must be available in the environment."
   )
   process.exit(1)
 }
@@ -26,7 +28,26 @@ const formattedVersions = (output[1].toString('utf8').match(/(\s-.*)*/g) || [])
   .join('')
   .replace(/\s-\s/g, '\n- ')
 
-const changes = `Packages pending updates:\n${formattedVersions}\n\nIf this is not what you expected, ensure that your commit messages follow the TDS commit types guide on this page: https://tds.telus.com/contributing/developer-guide.html and try again.`
+const links = `
+Links:
+
+- [Cartesian Components](http://telus-design-system-docs.s3-website-us-east-1.amazonaws.com/pr/${
+  process.env.CIRCLE_BRANCH
+}/e2e/)
+`
+
+const changes = `
+Packages pending updates:\n${formattedVersions}\n\nIf this is not what you expected, ensure that your commit messages follow the TDS commit types guide on this page: https://tds.telus.com/contributing/developer-guide.html and try again.
+`
+
+const body = `
+${links}
+
+\`\`\`
+${changes.trim()}
+\`\`\`
+`
+
 request.post({
   url: `https://api.github.com/repos/telus/${process.env.CIRCLE_PROJECT_REPONAME}/issues/${
     /[^/]*$/.exec(process.env.CIRCLE_PULL_REQUEST)[0]
@@ -37,8 +58,6 @@ request.post({
   },
   json: true,
   body: {
-    body: `\`\`\`
-${changes.trim()}
-\`\`\``,
+    body,
   },
 })
