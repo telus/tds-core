5e78c31f4ea7c54bc26d4bb352aaaa5b9b30d2fb
Jordan Raffoul <me@jordanraffoul.com>
2020-03-24 17:14:41 -0400

    chore(e2e): add home page listing all components

--- e2e/visual/index.jsx
+++ e2e/visual/index.jsx
@@ -1,13 +1,17 @@
 /* eslint-disable */
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { HashRouter as Router, Route } from 'react-router-dom'
+import { HashRouter as Router, Route, Link } from 'react-router-dom'
 import { createGlobalStyle } from 'styled-components'
 import StyledContainer from './shared/StyledContainer'
 import { createBrowserHistory } from 'history'
 
 import CSSReset from '../../packages/css-reset/index'
 import Heading from '../../packages/Heading'
+import FlexGrid from '../../packages/FlexGrid'
+import UnorderedList from '../../packages/UnorderedList'
+import Box from '../../packages/Box'
+import TDSLink from '../../packages/Link'
 
 import CartesianComponents from './components/**/*'
 
@@ -26,6 +30,27 @@ const renderExample = options => () => (
   </div>
 )
 
+const Home = () => (
+  <FlexGrid>
+    <FlexGrid.Row>
+      <FlexGrid.Col>
+        <Box between={3}>
+          <Heading level="h1">Cartesian Components</Heading>
+          <UnorderedList>
+            {CartesianComponents.map(CartesianComponent => (
+              <UnorderedList.Item>
+                <TDSLink reactRouterLinkComponent={Link} to={`/${CartesianComponent.default.name}`}>
+                  {CartesianComponent.default.name}
+                </TDSLink>
+              </UnorderedList.Item>
+            ))}
+          </UnorderedList>
+        </Box>
+      </FlexGrid.Col>
+    </FlexGrid.Row>
+  </FlexGrid>
+)
+
 const App = () => {
   const history = createBrowserHistory()
 
@@ -39,6 +64,7 @@ const App = () => {
       <CSSReset />
       <DisableAnimation />
       <Router>
+        <Route path="/" exact component={Home} />
         {CartesianComponents.map(CartesianComponent => (
           <Route
             key={CartesianComponent.default.name}
