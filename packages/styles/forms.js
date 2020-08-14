b3c18074db8b5e237e9a591f48e457790957714c
canrozanes <37912128+canrozanes@users.noreply.github.com>
2020-08-14 11:13:55 -0400

    feat(shared-styles): remove buttonHeight and add baseButton styles
    BREAKING CHANGE: remove buttonHeight export

--- packages/styles/forms.js
+++ packages/styles/forms.js
@@ -1,8 +1,5 @@
 import { fontTelus } from '@tds/shared-typography'
-
-export const buttonHeight = {
-  height: '3.25rem', // 52px
-}
+import { media } from '@tds/core-responsive'
 
 export const inputHeight = {
   height: '3rem', // 48px
@@ -10,4 +7,29 @@ export const inputHeight = {
 
 export const font = {
   fontFamily: fontTelus,
+}
+
+export const baseButton = {
+  margin: 0,
+  padding: '0 2rem',
+  cursor: 'pointer',
+  background: 'none',
+  transition: 'background 0.2s',
+  display: 'flex',
+  width: '100%',
+  alignItems: 'center',
+  justifyContent: 'center',
+  textAlign: 'center',
+  minHeight: '3.25rem', // 52px
+  ...media.from('md').css({
+    display: 'inline-flex',
+    width: 'auto',
+    minWidth: '180px',
+  }),
+  // this is to fix the IE 11 bug to center text. For more info: https://github.com/philipwalton/flexbugs/issues/231
+  '&:after': {
+    content: `""`,
+    minHeight: 'inherit',
+    fontSize: 0,
+  },
 }
