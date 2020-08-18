42ea1e82c49bc696355f4024f0bacedbacfcc9d6
Jordan Raffoul <me@jordanraffoul.com>
2020-08-18 10:50:03 -0400

    test(e2e): update CartesianButtton to include multiline example

--- e2e/visual/components/CartesianButton.jsx
+++ e2e/visual/components/CartesianButton.jsx
@@ -6,13 +6,26 @@ import { Cartesian } from '@compositor/kit'
 import Button from '../../../packages/Button'
 
 const CartesianButton = props => (
-  <Cartesian
-    {...props}
-    component={Button}
-    type={['button', 'submit', 'reset']}
-    variant={['primary', 'secondary', 'inverted']}
-    children={['Hello, world!']}
-  />
+  <div>
+    <div>
+      <Cartesian
+        {...props}
+        component={Button}
+        type={['button', 'submit', 'reset']}
+        variant={['primary', 'secondary', 'inverted']}
+        children={['Hello, world!']}
+      />
+    </div>
+    <div style={{ width: 200 }}>
+      <Cartesian
+        {...props}
+        component={Button}
+        type="button"
+        variant={['primary', 'secondary', 'inverted']}
+        children={[`Pre-order Galaxy Note20 Ultra 5G `]}
+      />
+    </div>
+  </div>
 )
 
 export default { name: 'Button', Component: CartesianButton }
