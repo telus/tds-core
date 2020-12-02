c64431ddc4a7c5e3e70d4c800d9343d95126ac4d
Mauricio Moreira <mmoreira@thoughtworks.com>
2020-12-02 17:33:50 -0300

    test(e2e): update CartesianButtonLink with new variants

--- e2e/visual/components/CartesianButtonLink.jsx
+++ e2e/visual/components/CartesianButtonLink.jsx
@@ -6,14 +6,25 @@ import { Cartesian } from '@compositor/kit'
 import ButtonLink from '../../../packages/ButtonLink'
 
 const CartesianButtonLink = props => (
-  <Cartesian
-    {...props}
-    component={ButtonLink}
-    variant={['primary', 'secondary', 'inverted']}
-    href="http://tds.telus.com"
-    children="TELUS Design System"
-    fullWidth={[false, true]}
-  />
+  <div>
+    <Cartesian
+      {...props}
+      component={ButtonLink}
+      variant={['primary', 'secondary', 'inverted']}
+      href="http://tds.telus.com"
+      children="TELUS Design System"
+      fullWidth={[false, true]}
+    />
+    <Cartesian
+      {...props}
+      component={ButtonLink}
+      variant={['standard', 'brand', 'danger', 'inverted']}
+      rank={['common', 'main']}
+      href="http://tds.telus.com"
+      children="NEW variants and ranks"
+      fullWidth={[false, true]}
+    />
+  </div>
 )
 
 export default { name: 'ButtonLink', Component: CartesianButtonLink }
