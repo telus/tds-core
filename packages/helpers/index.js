39b8594c365a62c75bb3e27d65687b75c35945fe
Jordan Raffoul <me@jordanraffoul.com>
2021-01-14 12:12:09 -0500

    feat(util-helpers): add pixelToRem()

--- packages/helpers/index.js
+++ packages/helpers/index.js
@@ -2,6 +2,7 @@
 export { default as getCopy } from './getCopy'
 export { default as uniqueId } from './uniqueId'
 export { default as safeRest } from './safeRest'
+export { default as pixelToRem } from './pixelToRem'
 export { default as DependentIconSizeContext } from './DependentIconSizeContext'
 export {
   default as generateResponsiveStyles,
---
+++ packages/helpers/pixelToRem.js
@@ -0,0 +1,7 @@
+const BASE_FONT_SIZE = 16
+
+const pixelToRem = pixel => {
+  return `${pixel / BASE_FONT_SIZE}rem`
+}
+
+export default pixelToRem
