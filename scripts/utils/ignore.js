a77ef985bd6bd5c0eb305250f1a29fbd9d31486e
Jordan Raffoul <me@jordanraffoul.com>
2021-02-16 13:12:59 -0500

    chore(packages): remove StandaloneIcon

--- scripts/utils/ignore.js
+++ scripts/utils/ignore.js
@@ -1,3 +1,3 @@
 // Lists of TDS components to ignore during builds, tests, and publishes
 
-module.exports = ['@tds/core-selector-counter']
+module.exports = ['@tds/core-selector-counter', '@tds/core-standalone-icon']
