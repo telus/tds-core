84b4db625331fbe2eb6faf22bf5b61dafd4e3dfc
Bill Somen <bill.somen@telus.com>
2021-11-30 07:17:46 -0500

    wip: tmp colourText

--- shared/components/ColoredTextProvider/ColoredTextProvider.jsx
+++ shared/components/ColoredTextProvider/ColoredTextProvider.jsx
@@ -2,16 +2,40 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 export const ColoredTextContext = React.createContext({
-  inheritColor: false,
+  inheritColor: undefined,
 })
 
 const ColoredTextProvider = ({ colorClassName, className, tag, children }) => {
   return (
-    <ColoredTextContext.Provider value={true}>
+    <ColoredTextContext.Provider value={{ inheritColor: true }}>
       {React.createElement(tag, { className: colorClassName || className }, children)}
     </ColoredTextContext.Provider>
   )
 }
+
+const ColoredTextProvider2 = ({ children }) => (
+  <ColoredTextContext.Provider value={{ inheritColor: true }}>
+    {children}
+  </ColoredTextContext.Provider>
+)
+
+// const withBasicProvider = (Provider) => (Component) => (props) => (
+//   <Provider>
+//     <Component {...props} />
+//   </Provider>
+// );
+
+// export default withBasicProvider(CounterProvider)(WrappedApp)
+
+export const WrappedColoredTextProvider = () => (
+  <ColoredTextProvider2>
+    <ColoredTextProvider />
+  </ColoredTextProvider2>
+  // // <ColoredTextContext.Provider value={{ inheritColor: true }}></ColoredTextContext.Provider>
+  // <ColoredTextProvider>
+  //   {children}
+  // </ColoredTextProvider>
+);
 
 ColoredTextProvider.propTypes = {
   colorClassName: PropTypes.string,
