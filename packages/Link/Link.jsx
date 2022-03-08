4b1d770566f03f6af6899ef8c1aa2606a417d26d
billsomen <bill.somen@telus.com>
2022-03-08 16:32:08 +0100

    fix(core-link): notification link issue in firefox

--- packages/Link/Link.jsx
+++ packages/Link/Link.jsx
@@ -37,6 +37,7 @@ const states = ({ invert }) => {
       border: `0.125rem solid ${invert ? colorWhite : colorGreyRaven}`,
       padding: '0.125rem',
       margin: '-0.25rem', // (border + padding) * -1
+      '-moz-margin-end': '0 !important', // Firefox only
       borderRadius: '0.25rem',
       outline: 'none',
     },
