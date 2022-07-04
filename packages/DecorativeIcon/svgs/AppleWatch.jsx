77c1e1096247a5612e116a92c22089a9fc18f102
billsomen <bill.somen@telus.com>
2022-07-04 15:11:36 +0100

    feat(core-decorative-icon): apple watch icon added

---
+++ packages/DecorativeIcon/svgs/AppleWatch.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import SVGIcon from '../SVGIcon'
+
+const AppleWatch = props => (
+  <SVGIcon {...props}>
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        fill="#fff"
+        d="M17 19.7143H6.71429C5.768 19.7143 5 18.9463 5 18V6.85711C5 5.91082 5.768 5.14282 6.71429 5.14282H17C17.9463 5.14282 18.7143 5.91082 18.7143 6.85711V18C18.7143 18.9463 17.9463 19.7143 17 19.7143Z"
+        stroke="#4B286D"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M7 5L7.61865 1H16.3814L17 5"
+        fill="#fff"
+        stroke="#4B286D"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M17 20L16.5217 23H7.47924L7 20"
+        fill="#fff"
+        stroke="#4B286D"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M19.5703 8.57153V10.2858"
+        fill="#fff"
+        stroke="#4B286D"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M19.5703 13.7144V15.4286"
+        fill="#fff"
+        stroke="#4B286D"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  </SVGIcon>
+)
+
+AppleWatch.displayName = 'DecorativeIcon'
+
+export default AppleWatch
