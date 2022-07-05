01542cff0ef12f1fbc072198cb7989eb0b02a3be
billsomen <bill.somen@telus.com>
2022-07-05 18:11:06 +0100

    fix(core-video): screen reader play, pause, replay update

--- packages/Video/ControlBar/Controls/VideoButton/VideoButton.jsx
+++ packages/Video/ControlBar/Controls/VideoButton/VideoButton.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 import styled from 'styled-components'
 
 const StyledButton = styled.button({
-  background: 'none',
+  background: 'red!important',
   color: 'inherit',
   border: 'none',
   padding: 0,
