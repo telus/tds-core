9bc0a02ec8df2a36460dd5dabba036e15ba10665
Shahzaib Khalid <shahzaibkhalidmalik@outlook.com>
2023-03-28 09:51:24 -0400

    Update deploy-docs.js

--- scripts/deploy-docs.js
+++ scripts/deploy-docs.js
@@ -75,12 +75,9 @@ const deployToS3Deprecated = () => {
   })
 }
 
-if (env === 'production') {
-  deployToS3('latest')
-    .then(() => deployToS3(`v${version}`))
-    // Continue to deploy to the thorium bucket because http://tds.telus.com points there
-    // TODO: Rip this out when the domain name is pointed at the new bucket: TDS-286
-    .then(() => deployToS3Deprecated())
-} else {
-  deployToS3('staging')
-}
+
+deployToS3('latest')
+  .then(() => deployToS3(`v${version}`))
+  // Continue to deploy to the thorium bucket because http://tds.telus.com points there
+  // TODO: Rip this out when the domain name is pointed at the new bucket: TDS-286
+  .then(() => deployToS3Deprecated())
